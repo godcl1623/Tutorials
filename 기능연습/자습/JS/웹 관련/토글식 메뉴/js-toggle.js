93c7109f39d48e8f111e92934295c6de500ab64f
taraczel <delphi5281@gmail.com>
2021-02-14 00:23:13 +0900

    자습 - 드림코딩 반응형 navbar 튜토리얼

--- 기능연습/자습/JS/웹 관련/토글식 메뉴/js-toggle.js
+++ 기능연습/자습/JS/웹 관련/토글식 메뉴/js-toggle.js
@@ -2,11 +2,21 @@ const trigger = document.querySelector('.mobile-menu');
 
 function toggleMenu(e) {
   const hiddenOne = document.querySelector('.nav-menu');
-  if (hiddenOne.classList.contains('hidden')) {
-    hiddenOne.classList.remove('hidden');
+  if (hiddenOne.classList.contains('active')) {
+    hiddenOne.classList.remove('active');
   } else {
-    hiddenOne.classList.add('hidden');
+    hiddenOne.classList.add('active');
+  }
+}
+
+function toggleMenu2(e) {
+  const hiddenTwo = document.querySelector('.icon-container');
+  if (hiddenTwo.classList.contains('active')) {
+    hiddenTwo.classList.remove('active');
+  } else {
+    hiddenTwo.classList.add('active');
   }
 }
 
 trigger.addEventListener('click', toggleMenu);
+trigger.addEventListener('click', toggleMenu2);
