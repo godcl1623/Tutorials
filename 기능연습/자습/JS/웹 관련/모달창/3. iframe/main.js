00357f6fa8314522caf8f6d583ba824403888af8
taraczel <delphi5281@gmail.com>
2021-02-21 00:45:07 +0900

    iframe 모달창 연습 - 자바스크립트 까지

--- 기능연습/자습/JS/웹 관련/모달창/3. iframe/main.js
+++ 기능연습/자습/JS/웹 관련/모달창/3. iframe/main.js
@@ -1,4 +1,12 @@
-const button = document.querySelector('.modal');
-button.addEventListener('click', (e) => {
+document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('div').style.display = 'block';
+  document.querySelector('iframe').style.display = 'block';
+});
+
+const iframeEle = document.querySelector('iframe');
+const iframeDocument = iframeEle.contentWindow.document;
+const iframeP = iframeDocument.getElementById('close');
+
+iframeP.addEventListener('click', (e) => {
   console.log(e);
 });
