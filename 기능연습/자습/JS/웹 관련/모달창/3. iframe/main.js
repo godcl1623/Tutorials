811a67a11e058d3f743d60707530680c752baf7f
taraczel <delphi5281@gmail.com>
2021-02-21 23:40:36 +0900

    main.*: iframe 관련 코드 미작동 원인 탐색 // iframe.*: iframe 파일 구성 개선

--- 기능연습/자습/JS/웹 관련/모달창/3. iframe/main.js
+++ 기능연습/자습/JS/웹 관련/모달창/3. iframe/main.js
@@ -1,12 +1,15 @@
-document.querySelector('button').addEventListener('click', () => {
-  document.querySelector('div').style.display = 'block';
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  // document.querySelector('div').style.display = 'block';
   document.querySelector('iframe').style.display = 'block';
 });
 
 const iframeEle = document.querySelector('iframe');
 const iframeDocument = iframeEle.contentWindow.document;
-const iframeP = iframeDocument.getElementById('close');
+const iframeP = iframeDocument.querySelector('p');
 
-iframeP.addEventListener('click', (e) => {
+/*iframeP.addEventListener('click', (e) => {
   console.log(e);
 });
+*/
