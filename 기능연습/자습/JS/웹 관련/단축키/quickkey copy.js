76aaa1c5fd9c8357153616061b7c4a57e5485f1b
taraczel <delphi5281@gmail.com>
2021-02-28 22:49:38 +0900

    단축키 복습 준비

---
+++ 기능연습/자습/JS/웹 관련/단축키/quickkey copy.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+const sidebar = document.querySelector('aside');
+const menu1 = document.getElementById('menu1');
+
+window.onscroll = function () {
+  function sidebarMove() {
+    if (window.pageYOffset < 189) {
+      sidebar.style.top = '189px';
+    } else if (window.pageYOffset >= 189 && window.pageYOffset < sidebar.offsetTop) {
+      sidebar.style.top = window.pageYOffset + 'px';
+    } else if ((window.pageYOffset + window.innerHeight) > (sidebar.offsetTop + sidebar.scrollHeight)) {
+      const top = window.pageYOffset + window.innerHeight - sidebar.scrollHeight - 2;
+      sidebar.style.top = top + 'px';
+    }
+  }
+  sidebarMove();
+};
+
+sidebar.addEventListener('scroll', onscroll);
+window.addEventListener('keydown', keyInput);
