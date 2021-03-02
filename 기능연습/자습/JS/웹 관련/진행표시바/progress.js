77efed4001e1ac7bae6bead84714638e748f1643
taraczel <delphi5281@gmail.com>
2021-03-03 00:38:08 +0900

    진행바 개선 및 프로젝트 완료

--- 기능연습/자습/JS/웹 관련/진행표시바/progress.js
+++ 기능연습/자습/JS/웹 관련/진행표시바/progress.js
@@ -1,17 +1,10 @@
 const progressFilled = document.querySelector('.progressive-filled');
-const progressMargin = parseFloat(window.getComputedStyle(progressFilled).marginRight.split('p').find(item => item.includes('1092')));
-const htmlHeight = document.querySelector('html').scrollHeight;
+const html = document.querySelector('html');
+const htmlHeight = html.scrollHeight - html.clientHeight;
 
 const progress = () => {
-  const scrollDiff = htmlHeight - (window.pageYOffset + window.innerHeight);
-  const scrollPercent = (scrollDiff / htmlHeight) * 100;
-  if (window.pageYOffset < window.innerHeight) {
-    progressFilled.style.display = 'none';
-  } else {
-    progressFilled.style.display = 'block';
-    progressFilled.style.marginRight = `${scrollPercent}%`;
-  }
-  // console.log(scrollPercent + 7.73);
+  const scrollPercent = (html.scrollTop / htmlHeight) * 100;
+  progressFilled.style.width = `${scrollPercent}%`;
 };
 
 window.addEventListener('scroll', progress);
