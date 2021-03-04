326d40b6425d9084a0e0d83d8c277a51f7ebfb86
taraczel <delphi5281@gmail.com>
2021-03-04 23:49:05 +0900

    드림코딩 배열api 강의자료 및 종합복습 2회차 - 단축키 토글메뉴 복습 완료, 클릭 토글메뉴+팝업창 여닫기 구현

--- 기능연습/자습/JS/웹 관련/종합 복습/2회차/review2.js
+++ 기능연습/자습/JS/웹 관련/종합 복습/2회차/review2.js
@@ -9,14 +9,50 @@
 
 'use strict';
 
-// 상수 및 변수
+// 전역 상수 및 변수
 const menuList = document.querySelectorAll('.menu-list');
+const toggleButton = document.querySelectorAll('.toggle');
+const keyData = Array.from(menuList).map(attribute => attribute.dataset.key);
+const modalLayer = document.querySelector('.modal-layer');
+const modalWindow = document.querySelector('.modal-window');
+const iframeModal = document.querySelector('iframe');
 
 // 함수
-const openSubMenu = () => {
+const openSubMenu = e => {
+  const keyCode = String(e.keyCode);
+  if (!keyData.includes(keyCode)) return;
+  const matchingKey = Array.from(menuList).find(attribute => attribute.dataset.key === keyCode);
+  matchingKey.firstElementChild.classList.toggle('active');
+};
 
+const openSubMenu2 = e => {
+  if (keyData.includes(e.target.dataset.key)) {
+    e.target.firstElementChild.classList.toggle('active');
+  }
+};
+
+const openPopup = e => {
+  modalLayer.classList.add('active');
+  console.log(e.target);
+  if (e.target.classList.contains('modal-toggle')) {
+    modalWindow.classList.add('active');
+  } else if (e.target.classList.contains('popup-toggle')) {
+    modalLayer.classList.remove('active');
+    window.open('review2-pop.html', 'popup window', 'width=300, height=400');
+  } else if (e.target.classList.contains('iframe-toggle')) {
+    iframeModal.classList.add('active');
+  }
+};
+
+window.closeModal = () => {
+  modalLayer.classList.remove('active');
+  modalWindow.classList.remove('active');
+  iframeModal.classList.remove('active');
 };
 
 // 이벤트 리스너
-// menuList.forEach(menu => menu.addEventListener('keydown', openSubMenu));
 window.addEventListener('keydown', openSubMenu);
+menuList.forEach(menu => menu.addEventListener('click', openSubMenu2));
+toggleButton.forEach(button => button.addEventListener('click', openPopup));
+modalLayer.addEventListener('click', closeModal);
+modalWindow.querySelector('p').addEventListener('click', window.closeModal);
