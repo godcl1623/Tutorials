f74b119dacf07305e0936038e907c9f8ae069ed0
taraczel <delphi5281@gmail.com>
2021-03-07 01:00:09 +0900

    모달창 body 스크롤 제한, 레이어만 스크롤 - 원인 발견: modal content의 Position을 relative로 놔야 함

--- 기능연습/자습/JS/웹 관련/모달창/4. 스크롤 제한/scroll.js
+++ 기능연습/자습/JS/웹 관련/모달창/4. 스크롤 제한/scroll.js
@@ -4,12 +4,20 @@ const layer = document.querySelector('.modal-layer');
 const openModal = () => {
   layer.classList.add('active');
   document.querySelector('.modal-window').classList.add('active');
+  document.querySelector('html').classList.add('hide-scroll');
+  document.body.classList.add('hide-scroll');
 };
 
 const closeModal = () => {
   layer.classList.remove('active');
   document.querySelector('.modal-window').classList.remove('active');
+  document.body.classList.remove('hide-scroll');
+  document.querySelector('html').classList.remove('hide-scroll');
 };
 
 button.addEventListener('click', openModal);
 layer.addEventListener('click', closeModal);
+
+// document.getElementsByTagName('button')[0].addEventListener('click', function() {
+// 	document.getElementById('popup').style.display = 'block';
+// });
