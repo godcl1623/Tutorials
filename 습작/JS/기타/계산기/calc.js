7057d60340c1d54e8e5cfbd2f16b870354c2cf4b
taraczel <delphi5281@gmail.com>
2021-03-09 00:41:56 +0900

    계산기 - 숫자출력 구현중

--- 습작/JS/기타/계산기/calc.js
+++ 습작/JS/기타/계산기/calc.js
@@ -1,6 +1,7 @@
 // 변수, 상수 모음 => 상수 - 변수 순
 
 const buttons = document.querySelectorAll('button');
+const numBox = document.querySelector('.number-window');
 
 // 함수 모음
 
@@ -22,6 +23,25 @@ const keyInput = e => {
   }
 };
 
+const printInput = e => {
+  const keySet1 = Array.from(buttons).map(attribute => attribute.dataset.key1);
+  const keySet2 = Array.from(buttons).map(attribute => attribute.dataset.key2);
+  const keyDown = String(e.keyCode);
+  const matchingKey = Array.from(buttons) //
+    .find(attribute =>
+      attribute.dataset.key1 === keyDown || attribute.dataset.key2 === keyDown);
+  const matchingButton = document.querySelector(`button[data-key1='${e.keyCode}']`)
+    || document.querySelector(`button[data-key2='${e.keyCode}']`);
+
+  if (!keySet1.includes(keyDown) && !keySet2.includes(keyDown)) return;
+  if (matchingKey.dataset.key1 === keyDown || matchingKey.dataset.key2) {
+    for (let i = 0; i < matchingKey.length; i++) {
+      numBox.innerHTML(document.write('test'));
+    }
+  }
+};
+
 // 이벤트 리스너 모음
 
 window.addEventListener('keydown', keyInput);
+window.addEventListener('keydown', printInput);
