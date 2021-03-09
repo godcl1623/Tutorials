b60fef0419566cc235bae40bd46056fc9035ad7c
taraczel <delphi5281@gmail.com>
2021-03-09 23:56:43 +0900

    계산기 - 자바스크립트로 입력사항 input표시 기능 구현

--- 습작/JS/기타/계산기/calc.js
+++ 습작/JS/기타/계산기/calc.js
@@ -2,6 +2,7 @@
 
 const buttons = document.querySelectorAll('button');
 const numBox = document.querySelector('.number-input');
+const numButton = document.querySelectorAll('.number-button');
 
 // 함수 모음
 
@@ -23,25 +24,20 @@ const keyInput = e => {
   }
 };
 
-// const printInput = e => {
-//   const keySet1 = Array.from(buttons).map(attribute => attribute.dataset.key1);
-//   const keySet2 = Array.from(buttons).map(attribute => attribute.dataset.key2);
-//   const keyDown = String(e.keyCode);
-//   const matchingKey = Array.from(buttons) //
-//     .find(attribute =>
-//       attribute.dataset.key1 === keyDown || attribute.dataset.key2 === keyDown);
-//   const matchingButton = document.querySelector(`button[data-key1='${e.keyCode}']`)
-//     || document.querySelector(`button[data-key2='${e.keyCode}']`);
+const printInput = e => {
+  const keySet1 = Array.from(buttons).map(attribute => attribute.dataset.key1);
+  const keySet2 = Array.from(buttons).map(attribute => attribute.dataset.key2);
+  const keyDown = String(e.keyCode);
+  if (!keySet1.includes(keyDown) && !keySet2.includes(keyDown)) return;
+  numBox.value += e.key;
+};
 
-//   if (!keySet1.includes(keyDown) && !keySet2.includes(keyDown)) return;
-//   if (matchingKey.dataset.key1 === keyDown || matchingKey.dataset.key2) {
-//     for (let i = 0; i < matchingKey.length; i++) {
-//       numBox.innerHTML(document.write('test'));
-//     }
-//   }
-// };
+const clickedInput = e => {
+  numBox.value += e.target.innerText;
+}
 
 // 이벤트 리스너 모음
 
 window.addEventListener('keydown', keyInput);
 window.addEventListener('keydown', printInput);
+numButton.forEach(clicked => clicked.addEventListener('click', clickedInput));
