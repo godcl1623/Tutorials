351a63abe16b7724fde789a0a372f8dc446a5784
taraczel <delphi5281@gmail.com>
2021-03-11 22:48:55 +0900

    계산기 - 특수연산자 기능 추가 진행중

--- 습작/JS/기타/계산기/calc.js
+++ 습작/JS/기타/계산기/calc.js
@@ -6,7 +6,7 @@ const numBox = document.querySelector('.number-input');
 const numButton = document.querySelectorAll('.number-button');
 
 // 함수 모음
-
+  // 키 선택 효과
 const keyInput = e => {
   const keySet1 = Array.from(buttons).map(attribute => attribute.dataset.key1);
   const keySet2 = Array.from(buttons).map(attribute => attribute.dataset.key2);
@@ -32,25 +32,29 @@ const keyInput = e => {
         percent.classList.remove('active');
       });
       break;
-      case e.key === '*':
-        const multiply = document.querySelector('button[data-key3="*"]');
-        multiply.classList.add('active');
-        multiply.addEventListener('transitionend', () => {
-          multiply.classList.remove('active');
-        });
-        break;
+    case e.key === '*':
+      const multiply = document.querySelector('button[data-key3="*"]');
+      multiply.classList.add('active');
+      multiply.addEventListener('transitionend', () => {
+        multiply.classList.remove('active');
+      });
+      break;
+    case e.key === '-':
+      const minus = document.querySelector('button[data-key3="-"]');
+      minus.classList.add('active');
+      minus.addEventListener('transitionend', () => {
+        minus.classList.remove('active');
+      });
+    break;
     case (matchingKey.dataset.key1 === keyDown || matchingKey.dataset.key2 === keyDown):
       matchingButton.classList.add('active');
       matchingButton.addEventListener('transitionend', () =>
         matchingButton.classList.remove('active')
       );
       break;
-    case e.key === '*':
-      console.log('test');
-      break;
   }
 };
-
+  // 숫자창에 숫자 띄우기 - 키입력
 const printInput = e => {
   const keySet1 = Array.from(numButton).map(attribute => attribute.dataset.key1);
   const keySet2 = Array.from(numButton).map(attribute => attribute.dataset.key2);
@@ -77,7 +81,7 @@ const printInput = e => {
     }
   }
 };
-
+  // 숫자창에 숫자 띄우기 - 버튼
 const clickedInput = e => {
   switch (true) {
     default: numBox.value += e.target.innerText;
@@ -92,14 +96,14 @@ const clickedInput = e => {
       break;
   }
 };
-
+  // 연산 초기화 - 대상 모음
 const eraser = () => {
   const memory = document.querySelector('.calculator-body');
   memory.removeAttribute('data-first-value');
   memory.removeAttribute('data-second-value');
   memory.removeAttribute('data-operator');
 };
-
+  // 연산 초기화 - 버튼
 const inputReset = () => {
   const init = document.querySelector('.initialize');
   init.addEventListener('click', () => {
@@ -107,14 +111,14 @@ const inputReset = () => {
     eraser();
   });
 };
-
+  // 연산 초기화 - 키입력
 const buttonEvents = e => {
   if (e.keyCode === 27) {
     numBox.value = '';
     eraser();
   }
 };
-
+  // 연산 함수
 const calculate = (operator, firstVal, secondVal) => {
   switch (operator) {
     default: break;
@@ -130,13 +134,22 @@ const calculate = (operator, firstVal, secondVal) => {
       return parseFloat(firstVal) / parseFloat(secondVal);
   }
 };
-
+  // 연산결과 출력
 const calculator = e => {
   const operator = document.querySelectorAll('.operator');
   const memory = document.querySelector('.calculator-body');
+    // 연산결과 출력 - 버튼입력의 경우
   operator.forEach(clicked => clicked.addEventListener('click', e => {
     switch (true) {
       default: break;
+      case e.target.innerText === '%':
+        numBox.value = parseFloat(numBox.value) / 100;
+        eraser();
+        break;
+      case e.target.innerText === '+/-':
+        numBox.value = parseFloat(numBox.value) * -1;
+        eraser();
+        break;
       case numBox.value !== '' && !e.target.classList.contains('result'):
         memory.dataset.firstValue = numBox.value;
         memory.dataset.operator = e.target.innerText;
@@ -152,6 +165,7 @@ const calculator = e => {
         break;
     }
   }));
+    // 연산결과 출력 - 키입력의 경우
   window.addEventListener('keydown', e => {
     switch (true) {
       default: break;
