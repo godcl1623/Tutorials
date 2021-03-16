775d43da21cb4ae5886c8d511f791d6a5881e862
taraczel <delphi5281@gmail.com>
2021-03-16 23:06:48 +0900

    쇼핑몰 미니게임 - 버튼 기능 구현 오나료

--- 기능연습/튜토리얼/JS/드림코딩/쇼핑몰 미니게임/mall.js
+++ 기능연습/튜토리얼/JS/드림코딩/쇼핑몰 미니게임/mall.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable strict */
 
 'use strict';
@@ -11,12 +12,56 @@ const buttons = document.querySelectorAll('button');
 // 함수 모음
 
 const sortFunction = e => {
-  const buttonsArray = Array.from(buttons);
-  const buttonTypes = buttonsArray.map(button => button.className);
-  const matchingTypes = buttonTypes.find(button => button === e.target.className);
-  
-  if (e.target.className === matchingTypes) {
-    console.log('test');
+  const clothes = document.querySelectorAll('.clothes');
+  const clothesArr = Array.from(clothes);
+  //
+  switch (true) {
+    case e.target.className === 'tshirts': {
+      const tshirts = clothesArr.filter(lists => lists.classList.contains('tshirts'));
+      const notTshirts = clothesArr.filter(lists => !lists.classList.contains('tshirts'));
+      tshirts.forEach(lists => {lists.style.display = 'flex'});
+      notTshirts.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.className === 'pants': {
+      const pants = clothesArr.filter(lists => lists.classList.contains('pants'));
+      const notPants = clothesArr.filter(lists => !lists.classList.contains('pants'));
+      pants.forEach(lists => {lists.style.display = 'flex'});
+      notPants.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.className === 'skirts': {
+      const skirts = clothesArr.filter(lists => lists.classList.contains('skirts'));
+      const notSkirts = clothesArr.filter(lists => !lists.classList.contains('skirts'));
+      skirts.forEach(lists => {lists.style.display = 'flex'});
+      notSkirts.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.className === 'blue': {
+      const blue = clothesArr.filter(lists => lists.classList.contains('blue'));
+      const notBlue = clothesArr.filter(lists => !lists.classList.contains('blue'));
+      blue.forEach(lists => {lists.style.display = 'flex'});
+      notBlue.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.className === 'yellow': {
+      const yellow = clothesArr.filter(lists => lists.classList.contains('yellow'));
+      const notYellow = clothesArr.filter(lists => !lists.classList.contains('yellow'));
+      yellow.forEach(lists => {lists.style.display = 'flex'});
+      notYellow.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.className === 'pink': {
+      const pink = clothesArr.filter(lists => lists.classList.contains('pink'));
+      const notPink = clothesArr.filter(lists => !lists.classList.contains('pink'));
+      pink.forEach(lists => {lists.style.display = 'flex'});
+      notPink.forEach(lists => {lists.style.display = 'none'});
+      break;
+    }
+    case e.target.classList.contains('init'):
+      clothes.forEach(lists => {lists.style.display = 'flex'});
+      break;
+    default: break;
   }
 };
 
