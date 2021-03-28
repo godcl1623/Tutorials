84e509b914c78c05fba020bdb81ca202b0d1fb5c
taraczel <delphi5281@gmail.com>
2021-03-28 22:27:30 +0900

    달력 & ToDo - 연도 선택 버튼 기능 추가중 + 이벤트 리스너 함수 수정, xss 취약점 수정

--- 습작/JS/기타/달력+ToDo/calendar.js
+++ 습작/JS/기타/달력+ToDo/calendar.js
@@ -7,8 +7,8 @@ const yearButton = document.querySelector('.year');
 const monthButton = document.querySelector('.month');
 let currentYear = dateStandard.getFullYear();
 let currentMonth = dateStandard.getMonth();
-yearButton.innerText = currentYear;
-monthButton.innerText = currentMonth + 1;
+yearButton.textContent = currentYear;
+monthButton.textContent = currentMonth + 1;
 const isLeapYear = year => {
   return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0);
 };
@@ -19,47 +19,52 @@ const febLastDay = year => {
 const calendarGenerator = (year = currentYear, month = currentMonth) => {
   const lastDays = [31, febLastDay(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const calendarDates = document.querySelector('.calendar__dates');
-  calendarDates.innerHTML = '';
+  calendarDates.textContent = '';
   const firstDay = new Date(year, month, 1);
   for (let i = 0; i <= lastDays[month] + firstDay.getDay() - 1; i++) {
     const day = document.createElement('div');
     if (i >= firstDay.getDay()) {
-      day.innerText = i - firstDay.getDay() + 1;
+      day.textContent = i - firstDay.getDay() + 1;
     }
     calendarDates.appendChild(day);
   }
 };
 calendarGenerator();
 
-const monthToPrev = () => {
+function monthToPrev() {
   if (currentMonth > 0) {
-    monthButton.innerText = `${parseFloat(monthButton.innerText) - 1}`;
+    monthButton.textContent = parseFloat(monthButton.textContent) - 1;
     --currentMonth;
     calendarGenerator(currentYear, currentMonth);
   } else {
-    yearButton.innerText = parseFloat(yearButton.innerText) - 1;
-    monthButton.innerText = 12;
+    yearButton.textContent = parseFloat(yearButton.textContent) - 1;
+    monthButton.textContent = 12;
     currentMonth = 11;
     currentYear -= 1;
     calendarGenerator(currentYear, currentMonth);
   }
 };
 
-const monthToNext = () => {
+function monthToNext() {
   if (currentMonth < 11) {
-    monthButton.innerText = parseFloat(monthButton.innerText) + 1;
+    monthButton.textContent = parseFloat(monthButton.textContent) + 1;
     ++currentMonth;
     calendarGenerator(currentYear, currentMonth);
   } else {
-    yearButton.innerText = parseFloat(yearButton.innerText) + 1;
-    monthButton.innerText = 1;
+    yearButton.textContent = parseFloat(yearButton.textContent) + 1;
+    monthButton.textContent = 1;
     currentMonth = 0;
     currentYear += 1;
     calendarGenerator(currentYear, currentMonth);
   }
 };
 
+function yearSelector() {
+
+}
+
 const prevMonth = document.querySelector('.prev__month');
 const nextMonth = document.querySelector('.next__month');
 prevMonth.addEventListener('click', monthToPrev);
 nextMonth.addEventListener('click', monthToNext);
+yearButton.addEventListener('click', yearSelector);
