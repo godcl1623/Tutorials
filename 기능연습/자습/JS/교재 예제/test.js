1e00ff544544f1578be16a149331fc91d46603fc
taraczel <delphi5281@gmail.com>
2021-03-30 21:13:58 +0900

    드림코딩 자바스크립트 강의 복습

--- 기능연습/자습/JS/교재 예제/test.js
+++ 기능연습/자습/JS/교재 예제/test.js
@@ -2,25 +2,39 @@
 
 'use strict';
 
-function evenNumber() {
-	for (let i = 0; i <= 10; i++) {
-		if (i % 2 === 0) {
-			console.log(i);
+const calculator = (num1, operator, num2) => {
+	switch (operator) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			return num1 / num2;
+		case '%':
+			return num1 % num2;
+		default:
+			throw Error('Invalid Operator');
+	}
+};
+
+class User {
+	constructor(firstName, lastName, age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	// get age() {
+	// 	return this._age;
+	// }
+	set name(value) {
+		if (value < 0) {
+			throw Error('age cannot be negative');
 		} else {
-			continue;
+			this._age = value;
 		}
 	}
 }
 
-evenNumber();
-
-function untilEight() {
-	for (let i = 0; i <= 10; i++) {
-		if (i > 8) {
-			break;
-		}
-			console.log(i);
-	}
-}
-
-untilEight();
+const jobs = new User('Joshua', 'Lee', 28);
