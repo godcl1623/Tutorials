24f36e9df785ece07d348f35d5d15f55da00c03c
taraczel <delphi5281@gmail.com>
2021-04-03 21:17:50 +0900

    달력 & ToDo 리모델링 - 선택한 날짜 리스트 표시 기능 추가

--- 습작/JS/기타/달력+ToDo/calendar copy.js
+++ 습작/JS/기타/달력+ToDo/calendar copy.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+/* 상수 및 변수 모음 */
+
 const dateStandard = new Date();
 const yearButton = document.querySelector('.year');
 const monthButton = document.querySelector('.month');
@@ -15,6 +17,9 @@ let currentYear = dateStandard.getFullYear();
 let currentMonth = dateStandard.getMonth();
 yearButton.textContent = currentYear;
 monthButton.textContent = currentMonth + 1;
+
+/* 호출용 함수 모음 - 기본적으로 const - function 순 */
+
 const isLeapYear = year => {
   return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0);
 };
@@ -25,6 +30,82 @@ const febLastDay = year => {
 function toDoCurrentDate(e) {
   const todoDate = document.querySelector('.todo__date .this__date');
   todoDate.textContent = e.target.dataset.date;
+}
+
+const defaultDate = () => {
+  const todoDate = document.querySelector('.todo__date .this__date');
+  todoDate.textContent = `${dateStandard.getFullYear()}.${dateStandard.getMonth() + 1}.${dateStandard.getDate()}`;
+};
+
+function openYearSelector() {
+  yearContainer.classList.add('active');
+}
+
+function openMonthSelector() {
+  monthContainer.classList.add('active');
+}
+
+function closeContainer() {
+  yearContainer.classList.remove('active');
+  monthContainer.classList.remove('active');
+}
+
+function addLocalStorage(input) {
+  const todoDate = document.querySelector('.todo__date .this__date');
+  const key = todoDate.innerText.replace(/\n/g, '');
+  const valueContainer = JSON.parse(localStorage.getItem(key)) || [];
+  valueContainer.push(input);
+  localStorage.setItem(key, JSON.stringify(valueContainer));
+}
+
+function getLocalStorageKey() {
+  const days = document.querySelectorAll('.day');
+  const dateValues = Array.from(days).filter(attribute => attribute.dataset.date);
+  const valueMap = dateValues.map(element => element.dataset.date);
+  const storageKeys = Object.keys(localStorage);
+  const matchingKeys = valueMap.filter(values => storageKeys.includes(values));
+  return matchingKeys;
+}
+
+function deleteList() {
+  const todoDate = document.querySelector('.todo__date .this__date');
+  const key = todoDate.innerText.replace(/\n/g, '');
+  const currentKey = getLocalStorageKey().find(element => element === key);
+  const currentValueSet = localStorage.getItem(currentKey);
+  console.log(currentValueSet);
+  // return test;
+}
+
+/* 주요기능 모음 */
+
+function showSelectedList(event) {
+  const todoContainer = document.querySelector('.todo__container');
+  const selectedDate = event.target.dataset.date;
+  const selectedList = JSON.parse(localStorage.getItem(selectedDate));
+  todoContainer.innerHTML = '';
+  const $ul = document.createElement('ul');
+  $ul.classList.add('.todo__list__container');
+  if (!Object.keys(localStorage).includes(selectedDate)) return;
+  for (let i = 0; i < selectedList.length; i++) {
+    const $li = document.createElement('li');
+    $li.classList.add('todo__list__contents');
+    const $input = document.createElement('input');
+    $input.type = 'checkbox';
+    $input.classList.add('checkbox');
+    $input.addEventListener('change', () => {console.log('changed')});
+    const $p = document.createElement('p');
+    $p.classList.add('list__item');
+    $p.textContent = selectedList[i];
+    const $button = document.createElement('button');
+    $button.classList.add('delete');
+    $button.textContent = 'delete';
+    $button.addEventListener('click', deleteList);
+    $li.appendChild($input);
+    $li.appendChild($p);
+    $li.appendChild($button);
+    $ul.appendChild($li);
+  }
+  todoContainer.appendChild($ul);
 }
 
 const calendarGenerator = (year = currentYear, month = currentMonth) => {
@@ -48,6 +129,7 @@ const calendarGenerator = (year = currentYear, month = currentMonth) => {
   }
   const days = document.querySelectorAll('.day');
   days.forEach(day => day.addEventListener('click', toDoCurrentDate));
+  days.forEach(dayBox => dayBox.addEventListener('click', showSelectedList));
 };
 
 function monthToPrev() {
@@ -100,49 +182,37 @@ const selectMonth = () => {
   selectors.forEach(button => button.addEventListener('click', monthChanger));
 };
 
-const defaultDate = () => {
-  const todoDate = document.querySelector('.todo__date .this__date');
-  todoDate.textContent = `${dateStandard.getFullYear()}.${dateStandard.getMonth() + 1}.${dateStandard.getDate()}`;
+const defaultUI = () => {
+  if (localStorage.length !== 0) {
+    const todoDate = document.querySelector('.todo__date .this__date');
+    const key = todoDate.innerText.replace(/\n/g, '');
+    const todoContainer = document.querySelector('.todo__container');
+    const todayList = getLocalStorageKey().find(dateValue => dateValue === key);
+    const defaultContents = JSON.parse(localStorage.getItem(todayList));
+    const $ul = document.createElement('ul');
+    $ul.classList.add('.todo__list__container');
+    for (let i = 0; i < defaultContents.length; i++) {
+      const $input = document.createElement('input');
+      $input.type = 'checkbox';
+      $input.classList.add('checkbox');
+      $input.addEventListener('change', () => {console.log('changed')});
+      const $li = document.createElement('li');
+      $li.classList.add('todo__list__contents');
+      const $p = document.createElement('p');
+      $p.classList.add('list__item');
+      $p.textContent = defaultContents[i];
+      const $button = document.createElement('button');
+      $button.classList.add('delete');
+      $button.textContent = 'delete';
+      $button.addEventListener('click', deleteList);
+      $li.appendChild($input);
+      $li.appendChild($p);
+      $li.appendChild($button);
+      $ul.appendChild($li);
+    }
+    todoContainer.appendChild($ul);
+  }
 };
-
-function openYearSelector() {
-  yearContainer.classList.add('active');
-}
-
-function openMonthSelector() {
-  monthContainer.classList.add('active');
-}
-
-function closeContainer() {
-  yearContainer.classList.remove('active');
-  monthContainer.classList.remove('active');
-}
-
-function addLocalStorage(input) {
-  const todoDate = document.querySelector('.todo__date .this__date');
-  const key = todoDate.innerText.replace(/\n/g, '');
-  const valueContainer = JSON.parse(localStorage.getItem(key)) || [];
-  valueContainer.push(input);
-  localStorage.setItem(key, JSON.stringify(valueContainer));
-}
-
-function getLocalStorageKey() {
-  const days = document.querySelectorAll('.day');
-  const dateValues = Array.from(days).filter(attribute => attribute.dataset.date);
-  const valueMap = dateValues.map(element => element.dataset.date);
-  const storageKeys = Object.keys(localStorage);
-  const matchingKeys = valueMap.filter(values => storageKeys.includes(values));
-  return matchingKeys;
-}
-
-function deleteList() {
-  const todoDate = document.querySelector('.todo__date .this__date');
-  const key = todoDate.innerText.replace(/\n/g, '');
-  const currentKey = getLocalStorageKey().find(element => element === key);
-  const currentValueSet = localStorage.getItem(currentKey);
-  console.log(currentValueSet);
-  // return test;
-}
 
 function addList() {
   const todoContainer = document.querySelector('.todo__container');
@@ -171,40 +241,13 @@ function addList() {
   textInput.value = '';
 }
 
-const defaultUI = () => {
-  if (localStorage.length !== 0) {
-    const todoContainer = document.querySelector('.todo__container');
-    const defaultContents = JSON.parse(localStorage.getItem(getLocalStorageKey()));
-    const $ul = document.createElement('ul');
-    $ul.classList.add('.todo__list__container');
-    for (let i = 0; i < defaultContents.length; i++) {
-      const $input = document.createElement('input');
-      $input.type = 'checkbox';
-      $input.classList.add('checkbox');
-      $input.addEventListener('change', () => {console.log('changed')});
-      const $li = document.createElement('li');
-      $li.classList.add('todo__list__contents');
-      const $p = document.createElement('p');
-      $p.classList.add('list__item');
-      $p.textContent = defaultContents[i];
-      const $button = document.createElement('button');
-      $button.classList.add('delete');
-      $button.textContent = 'delete';
-      $button.addEventListener('click', deleteList);
-      $li.appendChild($input);
-      $li.appendChild($p);
-      $li.appendChild($button);
-      $ul.appendChild($li);
-    }
-    todoContainer.appendChild($ul);
-  }
-};
-
 function enterList(e) {
   if (e.keyCode === 13) {
     addList();
   }
 }
+
+/* 선언 모음 */
 
 calendarGenerator();
 selectYear();
