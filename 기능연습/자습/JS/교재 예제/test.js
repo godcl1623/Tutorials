64d8f726706bb06b2dde9fe0aec91209286e174c
taraczel <delphi5281@gmail.com>
2021-04-11 01:19:58 +0900

    구조 분해 할당 테스트

--- 기능연습/자습/JS/교재 예제/test.js
+++ 기능연습/자습/JS/교재 예제/test.js
@@ -1,40 +1,4 @@
-/* eslint-disable strict */
+const foo = { 'fizz-buzz': true };
+const { 'fizz-buzz': fizzBuzz } = foo;
 
-'use strict';
-
-const calculator = (num1, operator, num2) => {
-	switch (operator) {
-		case '+':
-			return num1 + num2;
-		case '-':
-			return num1 - num2;
-		case '*':
-			return num1 * num2;
-		case '/':
-			return num1 / num2;
-		case '%':
-			return num1 % num2;
-		default:
-			throw Error('Invalid Operator');
-	}
-};
-
-class User {
-	constructor(firstName, lastName, age) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-	}
-	// get age() {
-	// 	return this._age;
-	// }
-	set name(value) {
-		if (value < 0) {
-			throw Error('age cannot be negative');
-		} else {
-			this._age = value;
-		}
-	}
-}
-
-const jobs = new User('Joshua', 'Lee', 28);
+console.log(fizzBuzz); // "true"
