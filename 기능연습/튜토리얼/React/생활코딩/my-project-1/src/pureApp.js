cce965eec269565a439f22d089384ea9b0d0652e
taraczel <delphi5281@gmail.com>
2021-04-13 23:06:13 +0900

    생활코딩 리액트 강의 - 컴포넌트 만들기 실습

---
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/pureApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Subject extends Component {
+  render(){
+    return ();
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default App;
