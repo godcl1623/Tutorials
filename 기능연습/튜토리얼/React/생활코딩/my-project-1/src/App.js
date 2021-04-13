25c35d31c0fc061ef8c75acf3bc0d7ec91739a9b
taraczel <delphi5281@gmail.com>
2021-04-13 23:45:32 +0900

    생활코딩 리액트 강의 - 컴포넌트 작성 실습 완료

--- 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Subject extends Component {
+  render(){
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
+class Menu extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="1.html">CSS</a></li>
+          <li><a href="1.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Contents extends Component {
+  render () {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://ko.reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Subject></Subject>
+        <Menu></Menu>
+        <Contents></Contents>
       </div>
     )
   }
--- 기능연습/튜토리얼/React/생활코딩/my-project-1/src/pureApp.js
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/pureApp.js
@@ -3,14 +3,21 @@ import './App.css';
 
 class Subject extends Component {
   render(){
-    return ();
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
   }
 }
 
 class App extends Component {
   render() {
     return (
-
+      <div className="App">
+        <Subject></Subject>
+      </div>
     )
   }
 }
