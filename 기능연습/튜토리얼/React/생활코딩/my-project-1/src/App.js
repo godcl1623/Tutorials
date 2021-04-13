e84ba5fb43ba91bf9c902ca124bd6c42d06a5ce5
taraczel <delphi5281@gmail.com>
2021-04-14 00:49:29 +0900

    생활코딩 리액트 강의 - state 실습

--- 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
@@ -5,13 +5,21 @@ import Menu from './components/Menu';
 import Contents from './components/Contents';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject1: {title:'WEB', text:'World Wide Web!'},
+      subject2: {title:'React', text:'For UI'},
+      contents: {title:'HTML', desc:'HTML is HyperText Markup Language.'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" text="World Wide Web!"></Subject>
-        <Subject title="React" text="For UI"></Subject>
+        <Subject title={this.state.subject1.title} text={this.state.subject1.text}></Subject>
+        <Subject title={this.state.subject2.title} text={this.state.subject2.text}></Subject>
         <Menu></Menu>
-        <Contents title="HTML" desc="HTML is HyperText Markup Language."></Contents>
+        <Contents title={this.state.contents.title} desc={this.state.contents.desc}></Contents>
       </div>
     )
   }
