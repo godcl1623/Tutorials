d49836f18bba1d83ff5d07e470b51f22fc692e42
taraczel <delphi5281@gmail.com>
2021-04-14 23:18:26 +0900

    생활코딩 리액트 강의 - 페이지 종류 판별을 위한 state 추가

--- 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome',
+      welcome: {title: 'Welcome', desc: 'Hello, React !!'},
       subject1: {title:'WEB', text:'World Wide Web!'},
       subject2: {title:'React', text:'For UI'},
       contents: {title:'HTML', desc:'HTML is HyperText Markup Language.'},
@@ -19,12 +21,20 @@ class App extends Component {
     }
   }
   render() {
+    let _title, _desc;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.menu[0].title;
+      _desc = this.state.menu[0].desc;
+    }
     return (
       <div className="App">
         <Subject title={this.state.subject1.title} text={this.state.subject1.text}></Subject>
         <Subject title={this.state.subject2.title} text={this.state.subject2.text}></Subject>
         <Menu data={this.state.menu}></Menu>
-        <Contents title={this.state.contents.title} desc={this.state.contents.desc}></Contents>
+        <Contents title={_title} desc={_desc}></Contents>
       </div>
     )
   }
