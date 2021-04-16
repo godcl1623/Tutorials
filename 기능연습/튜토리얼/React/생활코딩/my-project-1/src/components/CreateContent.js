24e880b56ef3e64325c9bf75ab0df85f69080df4
taraczel <delphi5281@gmail.com>
2021-04-16 23:29:12 +0900

    생활코딩 리액트 강의 - form 구현

--- 기능연습/튜토리얼/React/생활코딩/my-project-1/src/components/CreateContent.js
+++ 기능연습/튜토리얼/React/생활코딩/my-project-1/src/components/CreateContent.js
@@ -5,8 +5,34 @@ class CreateContent extends Component {
     return (
       <article>
         <h2>Create</h2>
-        <form>
-          
+        <form
+          action="/submit_process"
+          method="post"
+          onSubmit={event => {
+            event.preventDefault();
+            alert('submit !');
+          }}
+        >
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+            ></input>
+          </p>
+          <p>
+            <textarea
+              name="desc"
+              placeholder="Description"
+            ></textarea>
+          </p>
+          <p>
+            <input
+              type="submit"
+              name="submit"
+              value="submit"
+            ></input>
+          </p>
         </form>
       </article>
     );
