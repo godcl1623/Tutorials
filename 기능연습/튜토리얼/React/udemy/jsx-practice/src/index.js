e95d483688cd640b5a93a88932ccf87fe8cede65
taraczel <delphi5281@gmail.com>
2021-04-23 01:16:33 +0900

    udemy 리액트 강의 - JSX 실습 완료

--- 기능연습/튜토리얼/React/udemy/jsx-practice/src/index.js
+++ 기능연습/튜토리얼/React/udemy/jsx-practice/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const returnText = () => {
+  return 'Click';
+}
+
 const App = () => {
-  return <div>Hello React !</div>;
+  const textLocation = 'Submit';
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button
+        style={{
+          backgroundColor: 'blue',
+          color: 'white'
+        }}
+      >
+        {/* {textLocation} */}
+        {returnText()}
+      </button>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
