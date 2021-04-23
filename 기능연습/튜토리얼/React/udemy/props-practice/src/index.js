89cfc78dac4bae5cb502331b6961fee43d5f6198
taraczel <delphi5281@gmail.com>
2021-04-23 23:15:07 +0900

    udemy 리액트 강의 - props 실습 - 컴포넌트 하드코딩 항목 전부 props로 대체 완료

--- 기능연습/튜토리얼/React/udemy/props-practice/src/index.js
+++ 기능연습/튜토리얼/React/udemy/props-practice/src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo="오후 4시 45분"
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo="어제"
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo="3일 전"
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo="1주일 전"
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo="1년 전"
+        comment={faker.lorem.sentence()}
+      />
     </div>
   );
 };
