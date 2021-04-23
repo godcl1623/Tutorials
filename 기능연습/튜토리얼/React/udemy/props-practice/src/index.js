97dd3a45170be089569e819ba5d0cd4147654343
taraczel <delphi5281@gmail.com>
2021-04-23 23:50:32 +0900

    udemy 리액트 강의 - props 실습 완료

--- 기능연습/튜토리얼/React/udemy/props-practice/src/index.js
+++ 기능연습/튜토리얼/React/udemy/props-practice/src/index.js
@@ -1,41 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="오후 4시 45분"
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="어제"
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="3일 전"
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="1주일 전"
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="1년 전"
-        comment={faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning !</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="오후 4시 45분"
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="어제"
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="3일 전"
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="1주일 전"
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="1년 전"
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
