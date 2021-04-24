cab2efdbe049620a406eda1f1c181f389d3ea24c
taraczel <delphi5281@gmail.com>
2021-04-25 01:11:24 +0900

    JS 습작 - 웹 관련 - 슬라이드쇼 - 1유형 완성

--- 습작/JS/웹 관련/슬라이드쇼/index.js
+++ 습작/JS/웹 관련/슬라이드쇼/index.js
@@ -1,21 +1,51 @@
 // 상수 모음
-const body = document.querySelector('body');
+const orders = document.querySelectorAll('.order');
+const images = document.querySelectorAll('img');
+const imgArray = Array.from(images);
+const buttons = document.querySelectorAll('button');
+let orderIndex = 0;
 // 함수 모음
-function divGenerator() {
-  for (let i = 0; i < 5; i++) {
-    const $container = document.createElement('div');
-    $container.classList.add('container');
-    const $textBox = document.createElement('p');
-    $textBox.classList.add('order');
-    const $img = document.createElement('img');
-    $img.classList.add('img');
-    const $desc = document.createElement('p');
-    $desc.classList.add('desc');
-    $container.appendChild($textBox);
-    $container.appendChild($img);
-    $container.appendChild($desc);
-    body.appendChild($container);
+const hideContainer = () => {
+  const containers = document.querySelectorAll('.container');
+  Array.from(containers).forEach(container => {
+    if (container.dataset.index === String(orderIndex + 1)) {
+      container.classList.remove('hide');
+    } else {
+      container.classList.add('hide');
+    }
+  });
+};
+
+const orderIndexer = () => {
+  Array.from(orders).forEach((order, i) => {
+    order.innerText = `${i + 1} / ${orders.length}`;
+  });
+};
+
+function minusIndex() {
+  if (orderIndex === 0) {
+    orderIndex = imgArray.length;
+  }
+  orderIndex--;
+}
+
+function plusIndex() {
+  if (orderIndex === imgArray.length - 1) {
+    orderIndex = -1;
+  }
+  orderIndex++;
+}
+
+function moveImage() {
+  if (this.className === 'prev') {
+    minusIndex();
+    hideContainer();
+  } else {
+    plusIndex();
+    hideContainer();
   }
 }
 // 이벤트 리스너 모음
-window.onload = () => {divGenerator()};
+hideContainer();
+orderIndexer();
+buttons.forEach(button => button.addEventListener('click', moveImage));
