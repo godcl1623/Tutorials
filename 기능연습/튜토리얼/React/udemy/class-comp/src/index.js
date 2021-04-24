da79d84248ef73f13b49412f1911ad047630beec
taraczel <delphi5281@gmail.com>
2021-04-24 22:41:42 +0900

    udemy 리액트 강의 - 클래스 컴포넌트, state 실습 완료

--- 기능연습/튜토리얼/React/udemy/class-comp/src/index.js
+++ 기능연습/튜토리얼/React/udemy/class-comp/src/index.js
@@ -15,17 +15,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: null
+      lat: null,
+      errMsg: ''
     }
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat: position.coords.latitude});
+      },
+      err => {
+        this.setState({errMsg: err.message});
+      }
+    );
   }
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-    return (
-      <div>Latitude: </div>
-    );
+    if (this.state.errMsg && !this.state.lat) {
+      return <div>Error: {this.state.errMsg}</div>
+    }
+  
+    if (!this.state.errMsg && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+  
+    return <div> Loading ! </div>
   }
 }
 
