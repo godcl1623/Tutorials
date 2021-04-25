e448b288c96530c96251c80b3a1c465f66bcee9a
taraczel <delphi5281@gmail.com>
2021-04-25 23:40:27 +0900

    자바스크립트 습작 - 슬라이드쇼 - dataset 자동 부여, 라디오버튼 추가

--- 습작/JS/웹 관련/슬라이드쇼/index.js
+++ 습작/JS/웹 관련/슬라이드쇼/index.js
@@ -3,10 +3,29 @@ const orders = document.querySelectorAll('.order');
 const images = document.querySelectorAll('img');
 const imgArray = Array.from(images);
 const buttons = document.querySelectorAll('button');
+const containers = document.querySelectorAll('.container');
 let orderIndex = 0;
 // 함수 모음
+const dataIndexer = target => {
+  Array.from(target).forEach((element, i) => {
+    element.dataset.index = i + 1;
+  });
+};
+
+const putRadioBtns = () => {
+  const body = document.querySelector('body');
+  const $div = document.createElement('div');
+  for (let i = 0; i < images.length ; i++) {
+    $div.classList.add('radioContainer');
+    const $input = document.createElement('input');
+    $input.type = 'radio';
+    $input.classList.add('imgOrder');
+    $div.appendChild($input);
+  }
+  body.appendChild($div);
+};
+
 const hideContainer = () => {
-  const containers = document.querySelectorAll('.container');
   Array.from(containers).forEach(container => {
     if (container.dataset.index === String(orderIndex + 1)) {
       container.classList.remove('hide');
@@ -46,6 +65,8 @@ function moveImage() {
   }
 }
 // 이벤트 리스너 모음
+dataIndexer(containers);
+putRadioBtns();
 hideContainer();
 orderIndexer();
 buttons.forEach(button => button.addEventListener('click', moveImage));
