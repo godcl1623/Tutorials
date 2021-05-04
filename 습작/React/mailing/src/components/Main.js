f5285357604d4d264a31105e38c601bcf42049a0
taraczel <delphi5281@gmail.com>
2021-05-05 00:06:58 +0900

    리액트 습작 - 메일링 서비스 - submit시 form 입력내용 객체로 저장 기능 구현, form span들 label로 수정중

--- 습작/React/mailing/src/components/Main.js
+++ 습작/React/mailing/src/components/Main.js
@@ -3,22 +3,45 @@ import './Main.css';
 
 class Main extends Component {
   selections = function() {
-    const container = [];
+    // const container = [];
+    const values = [];
     for (let i = 0; i < 8; i++) {
-      container.push(
-        <div key={i} className="selection-container">
-          <input id={`cb${i+1}`} type="checkbox" />
-          <p>lorem ipsum {i+1}</p>
-        </div>
-      )
+      // container.push(
+      //   <div key={i} className="selection-container">
+      //     <input id={`cb${i+1}`} type="checkbox" name="interest" />
+      //     <p>lorem ipsum {i+1}</p>
+      //   </div>
+      // )
+      values.push(`lorem ${i+1}`);
     }
-    return container;
+    const interests = values.map((value, i) => {
+      const interestForm = (
+        <div key={i} className="selection-container">
+          <input
+            id={`cb${i+1}`}
+            type="checkbox"
+            name="interest"
+            value={values[i]}
+          />
+          <p>lorem ipsum {i+1}</p>
+        </div>);
+      return interestForm;
+    });
+    return interests;
   };
 
   handleSubmit = event => {
     event.preventDefault();
     const data = new FormData(event.target);
-    const value = data.entries();
+    const value = {
+      name: data.get('name'),
+      family: data.get('family'),
+      gender: data.get('gender'),
+      email: `${data.get('email-id')}@${data.get('email-provider')}`,
+      source: data.get('source'),
+      interests: data.getAll('interest'),
+      favorite: data.get('favorite-time')
+    }
     console.log({value});
   }
 
@@ -30,26 +53,26 @@ class Main extends Component {
           method="post"
           onSubmit={(event) => {this.handleSubmit(event)}}
         >
-          <span className="name-header">이름</span>
-          <input className="name-input" type="text" name="name" />
-          <span className="family-header">성</span>
-          <input className="family-input" type="text" name="family" />
-          <span className="gender-header">성별</span>
-          <input className="gender-input" type="text" />
-          <span className="email-header">이메일</span>
+          <label className="name-header" htmlFor="name">이름</label>
+          <input className="name-input" type="text" name="name" defaultValue="bar" />
+          <label className="family-header" htmlFor="family">성</label>
+          <input className="family-input" type="text" name="family" defaultValue="foo" />
+          <label className="gender-header" htmlFor="gender">성별</label>
+          <input className="gender-input" type="text" name="gender" defaultValue="apache" />
+          <label className="email-header" htmlFor="email-input">이메일</label>
           <section className="email-input">
-            <input type="text" />
+            <input type="text" name="email-id" defaultValue="email" />
             <span>@</span>
-            <input type="text" />
+            <input type="text" name="email-provider" defaultValue="provider.com" />
           </section>
-            <span className="source-header">가입경로</span>
-            <textarea className="source-input" />
-              <span className="interest-header">관심사</span>
+            <label className="source-header" htmlFor="source">가입경로</label>
+            <textarea className="source-input" name="source" defaultValue="비밀" />
+              <label className="interest-header" htmlFor="">관심사</label>
               <section className="interest-input">
                 {this.selections()}
               </section>
-            <span className="favorite-header">희망 배송시간</span>
-            <textarea className="favorite-input" />
+            <label className="favorite-header" htmlFor="">희망 배송시간</label>
+            <textarea className="favorite-input" name="favorite-time" defaultValue="오전 14시" />
           <input
             className="submit"
             type="submit"
