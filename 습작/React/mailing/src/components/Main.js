539541f3c630ebe9ef64a061b66c3ec3c484946a
taraczel <delphi5281@gmail.com>
2021-05-05 00:19:46 +0900

    리액트 습작 - 메일링 서비스 - span 일부 label로 수정 완료

--- 습작/React/mailing/src/components/Main.js
+++ 습작/React/mailing/src/components/Main.js
@@ -6,17 +6,11 @@ class Main extends Component {
     // const container = [];
     const values = [];
     for (let i = 0; i < 8; i++) {
-      // container.push(
-      //   <div key={i} className="selection-container">
-      //     <input id={`cb${i+1}`} type="checkbox" name="interest" />
-      //     <p>lorem ipsum {i+1}</p>
-      //   </div>
-      // )
       values.push(`lorem ${i+1}`);
     }
     const interests = values.map((value, i) => {
       const interestForm = (
-        <div key={i} className="selection-container">
+        <div key={i} id="selection-container">
           <input
             id={`cb${i+1}`}
             type="checkbox"
@@ -53,32 +47,95 @@ class Main extends Component {
           method="post"
           onSubmit={(event) => {this.handleSubmit(event)}}
         >
-          <label className="name-header" htmlFor="name">이름</label>
-          <input className="name-input" type="text" name="name" defaultValue="bar" />
-          <label className="family-header" htmlFor="family">성</label>
-          <input className="family-input" type="text" name="family" defaultValue="foo" />
-          <label className="gender-header" htmlFor="gender">성별</label>
-          <input className="gender-input" type="text" name="gender" defaultValue="apache" />
-          <label className="email-header" htmlFor="email-input">이메일</label>
-          <section className="email-input">
-            <input type="text" name="email-id" defaultValue="email" />
-            <span>@</span>
-            <input type="text" name="email-provider" defaultValue="provider.com" />
-          </section>
-            <label className="source-header" htmlFor="source">가입경로</label>
-            <textarea className="source-input" name="source" defaultValue="비밀" />
-              <label className="interest-header" htmlFor="">관심사</label>
-              <section className="interest-input">
-                {this.selections()}
-              </section>
-            <label className="favorite-header" htmlFor="">희망 배송시간</label>
-            <textarea className="favorite-input" name="favorite-time" defaultValue="오전 14시" />
+          <label
+            className="name-header"
+            htmlFor="name-input"
+          >
+            이름
+          </label>
+            <input
+              id="name-input"
+              type="text"
+              name="name"
+              defaultValue="bar"
+            />
+          <label
+            className="family-header"
+            htmlFor="family-input"
+          >
+            성
+          </label>
+            <input
+              id="family-input"
+              type="text"
+              name="family"
+              defaultValue="foo"
+            />
+          <label
+            className="gender-header"
+            htmlFor="gender-input"
+          >
+            성별
+          </label>
+            <input
+              id="gender-input"
+              type="text"
+              name="gender"
+              defaultValue="apache"
+            />
+          <label
+            className="email-header"
+            htmlFor="email-input"
+          >
+            이메일
+          </label>
+            <section id="email-input">
+              <input
+                type="text"
+                name="email-id"
+                defaultValue="email"
+              />
+              <span>@</span>
+              <input
+                type="text"
+                name="email-provider"
+                defaultValue="provider.com"
+              />
+            </section>
+          <label
+            className="source-header"
+            htmlFor="source-input"
+          >
+            가입경로
+          </label>
+            <textarea
+              id="source-input"
+              name="source"
+              defaultValue="비밀"
+            />
+          <label
+            className="interest-header"
+            htmlFor="interest-input"
+          >
+            관심사
+          </label>
+            <section id="interest-input">
+              {this.selections()}
+            </section>
+          <label
+            className="favorite-header"
+            htmlFor="favorite-input"
+          >
+            희망 배송시간
+          </label>
+            <textarea
+              id="favorite-input"
+              name="favorite-time"
+              defaultValue="오전 14시"
+            />
           <input
-            className="submit"
+            id="submit"
             type="submit"
-            // onClick={event => {
-            //   event.preventDefault();
-            // }}
           />
         </form>
         {/* <button>관리</button> */}
