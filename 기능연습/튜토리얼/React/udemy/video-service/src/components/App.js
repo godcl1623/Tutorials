58db6859e5c121c6681c44cdcb24dc0e556c37b6
taraczel <delphi5281@gmail.com>
2021-05-05 23:28:15 +0900

    udemy 리액트 강의 - 검색기능 구현

--- 기능연습/튜토리얼/React/udemy/video-service/src/components/App.js
+++ 기능연습/튜토리얼/React/udemy/video-service/src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import YoutubeAPI from '../api/YoutubeAPI';
 
 class App extends React.Component {
+  state={ videos: [] }
+  onTermSubmit = async input => {
+    const response = await YoutubeAPI.get('/search', {
+      params: {
+        q: input
+      }
+    });
+    this.setState({
+      videos: response.data.items
+    })
+  };
   render() {
     return(
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar
+          onFormSubmit={this.onTermSubmit}
+        />
+        검색 결과: {this.state.videos.length} 건
       </div>
     );
   }
--- 기능연습/튜토리얼/React/udemy/video-service/src/components/SearchBar.js
+++ 기능연습/튜토리얼/React/udemy/video-service/src/components/SearchBar.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { text: '' }
+
+  changeText = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  submitQuery = event => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.text);
+  }
+
   render(){
     return (
-      <div>SearchBar</div>
+      <div className="search-bar ui segment">
+        <form
+          className="ui form"
+          onSubmit={this.submitQuery}
+        >
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.text}
+              onChange={this.changeText}
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
