afb92b01f8847325afbadbb6e3aac3efa4c86df0
taraczel <delphi5281@gmail.com>
2021-05-07 00:25:21 +0900

    udemy 리액트 강의 - hooks 중 useState를 이용한 accordion 컴포넌트 작성

--- 기능연습/튜토리얼/React/udemy/widget-practice/src/components/App.js
+++ 기능연습/튜토리얼/React/udemy/widget-practice/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'By creating components with React'
+  }
+];
 
 const App = () => {
   return(
-    <div>App</div>
+    <div>
+      <Accordion items={items} />
+    </div>
   );
 }
 
