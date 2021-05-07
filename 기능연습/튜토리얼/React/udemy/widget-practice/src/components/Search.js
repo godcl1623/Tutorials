9895818a72e0c86ded5c30999891f30f5a2e4e75
taraczel <delphi5281@gmail.com>
2021-05-08 01:00:20 +0900

    udemy 리액트 강의 - 위키피디아 api 작성중

--- 기능연습/튜토리얼/React/udemy/widget-practice/src/components/Search.js
+++ 기능연습/튜토리얼/React/udemy/widget-practice/src/components/Search.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const renderResult = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {/* {result.snippet} */}
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    const searchQuery = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+
+      setResults(data.query.search);
+    }
+
+    if (term !== '') {
+      searchQuery();
+    }
+  }, [term]);
+
   return(
     <div>
-      Search Component
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list">
+        {renderResult}
+      </div>
     </div>
   );
 }
