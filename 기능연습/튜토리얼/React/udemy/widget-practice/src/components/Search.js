28b8343e81f15ab061f34592dbee0edbf76ac649
taraczel <delphi5281@gmail.com>
2021-05-08 22:03:57 +0900

    udemy 리액트 강의 - 검색 쿼리 요청 메커니즘 개선

--- 기능연습/튜토리얼/React/udemy/widget-practice/src/components/Search.js
+++ 기능연습/튜토리얼/React/udemy/widget-practice/src/components/Search.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  // 기본 검색어를 지정하는 경우
+  const [term, setTerm] = useState('리액트');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  // 기본 검색어를 지정하지 않는 경우
+  // const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
 
   const renderResult = results.map(result => {
@@ -23,6 +27,45 @@ const Search = () => {
     );
   });
 
+  // 버전 1 - state term의 useState 기본값으로 빈 문자열('')을 사용한 경우에 사용
+  // useEffect(() => {
+  //   const searchQuery = async () => {
+  //     const { data } = await axios.get('https://ko.wikipedia.org/w/api.php', {
+  //       params: {
+  //         action: 'query',
+  //         list: 'search',
+  //         origin: '*',
+  //         format: 'json',
+  //         srsearch: term
+  //       }
+  //     });
+
+  //     setResults(data.query.search);
+  //   }
+
+  //   const timeoutId = setTimeout(() => {
+  //     if (term) {
+  //       searchQuery();
+  //     }
+  //   }, 500);
+
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   }
+  // }, [term]);
+
+  // 버전 2 - state term의 useState 기본값으로 빈 문자열이 아니라 특정 검색어를 사용한 경우에 사용
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const searchQuery = async () => {
       const { data } = await axios.get('https://ko.wikipedia.org/w/api.php', {
@@ -31,23 +74,15 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         }
       });
 
       setResults(data.query.search);
     }
 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        searchQuery();
-      }
-    }, 500);
-
-    return () => {
-      clearTimeout(timeoutId);
-    }
-  }, [term]);
+    searchQuery();
+  }, [debouncedTerm]);
 
   return(
     <div>
