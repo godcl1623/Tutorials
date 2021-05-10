25f37831aab58ec1ae55437afd3a60c5300ea781
taraczel <delphi5281@gmail.com>
2021-05-10 22:17:15 +0900

    udemy 리액트 강의 - 라우팅 기능 구현

--- 기능연습/튜토리얼/React/udemy/widget-practice/src/components/App.js
+++ 기능연습/튜토리얼/React/udemy/widget-practice/src/components/App.js
@@ -3,6 +3,7 @@ import Accordion from './Accordion';
 import Search from './Search';
 import Dropdown from './Dropdown';
 import Translate from './Translate';
+import Route from './Route';
 
 const items = [
   {
@@ -34,9 +35,30 @@ const options = [
   }
 ];
 
+// const router = () => {
+//   switch(window.location.pathname) {
+//     case '/search':
+//       return <Search />;
+//     // case '/dropdown':
+//     //   return(
+//     //     <Dropdown
+//     //       label='Select a Color'
+//     //       options={options}
+//     //       selected={selected}
+//     //       onSelectedChange={setSelected}
+//     //       example='폰트 색상 미리보기'
+//     //     />
+//     //   );
+//     case '/translate':
+//       return <Translate />;
+//     default:
+//       return <Accordion items={items} />;
+//   }
+// }
+
 const App = () => {
-  // const [selected, setSelected] = useState('');
-  // const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState('');
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return(
     <div>
@@ -52,7 +74,26 @@ const App = () => {
           example='폰트 색상 미리보기'
         /> : null
       } */}
-      <Translate />
+      {/* <Translate /> */}
+      {/* {router()} */}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label='Select a Color'
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+          example='폰트 색상 미리보기'
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
