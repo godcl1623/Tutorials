f8d9f92a0961a7d31995e701729960876ccdba72
taraczel <delphi5281@gmail.com>
2021-05-10 23:43:41 +0900

    udemy 리액트 강의 - 라우팅을 통한 내비게이션 기능 개선

---
+++ 기능연습/튜토리얼/React/udemy/widget-practice/src/components/Link.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Link = ({ href, className, children }) => {
+  const onClickAnchor = event => {
+    if (event.metaKey || event.ctrlKey) return;
+
+    event.preventDefault();
+    window.history.pushState({}, '', href);
+    
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  }
+
+  return(
+    <a
+      href={href}
+      className={className}
+      onClick={onClickAnchor}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
