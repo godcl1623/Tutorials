c3a3a57a22dc9fe506d31052ba479fe1776c441c
taraczel <delphi5281@gmail.com>
2021-05-17 00:48:39 +0900

    udemy react+redux - 블로그 - api 데이터로 리스트 구성

--- 기능연습/튜토리얼/React+Redux/blog/src/components/PostList.js
+++ 기능연습/튜토리얼/React+Redux/blog/src/components/PostList.js
@@ -7,10 +7,25 @@ const PostList = ({fetchPosts, posts}) => {
     fetchPosts();
   }, [fetchPosts]);
 
-  console.log(posts);
+  const renderList = () => {
+    return posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return(
-    <div>
-      <h1>PostList</h1>
+    <div className="ui relaxed divided list">
+      {renderList()}
     </div>
   );
 };
