5ee8fcce7f19d8bf0f192b0442828042e708009c
taraczel <delphi5281@gmail.com>
2021-05-18 18:20:31 +0900

    udemy react+redux - userHeader의 mapStateToProps에 로직 적용으로 인한 코드 개선

--- 기능연습/튜토리얼/React+Redux/blog/src/components/UserHeader.js
+++ 기능연습/튜토리얼/React+Redux/blog/src/components/UserHeader.js
@@ -2,12 +2,10 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
-const UserHeader = ({ fetchUser, userId, users }) => {
+const UserHeader = ({ fetchUser, userId, user }) => {
   useEffect(() => {
     fetchUser(userId);
   }, [fetchUser, userId]);
-
-  const user = users.find(user => user.id === userId);
 
   if (!user) {
     return null;
@@ -20,8 +18,8 @@ const UserHeader = ({ fetchUser, userId, users }) => {
   );
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
