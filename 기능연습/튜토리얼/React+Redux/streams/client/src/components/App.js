86108b6a32fa10d3bb246018247fb9a3b1e7fe7e
taraczel <delphi5281@gmail.com>
2021-05-20 00:26:08 +0900

    udemy react+redux - 내비게이터 메뉴 생성

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/App.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Navigator from './Navigator';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -8,9 +9,10 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Navigator />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
