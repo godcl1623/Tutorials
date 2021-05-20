e1aa04523db0f3d8b0990e9dcc00fe7665637e66
taraczel <delphi5281@gmail.com>
2021-05-21 00:31:26 +0900

    udemy react+redux - 스트리밍 앱 - 로그인 버튼, 로그아웃 버튼 구현 및 버튼에 핸들러 추가

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/GoogleAuth.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/GoogleAuth.js
@@ -2,26 +2,61 @@ import React, { useState, useEffect } from 'react';
 
 const GoogleAuth = () => {
   const [isSignedIn, setIsSignedIn] = useState(null);
+
   useEffect(() => {
     window.gapi.load('client:auth2', () => {
+      const onAuthChange = () => {
+        const auth = window.gapi.auth2.getAuthInstance();
+        setIsSignedIn(auth.isSignedIn.get());
+      };
+
       window.gapi.client.init({
         clientId: '1026008969674-urgnfp54da0a0rot6ggevesnehkqndas.apps.googleusercontent.com',
         scope: 'email'
       }).then(() => {
         const auth = window.gapi.auth2.getAuthInstance();
         setIsSignedIn(auth.isSignedIn.get());
+        auth.isSignedIn.listen(onAuthChange);
       });
     });
   }, []);
 
+  const makeSignIn = () => {
+    const auth = window.gapi.auth2.getAuthInstance();
+    auth.signIn();
+    console.log('the user is signed in !');
+  };
+
+  const makeSignOut = () => {
+    const auth = window.gapi.auth2.getAuthInstance();
+    auth.signOut();
+    console.log('the user is signed out !');
+  };
+
   const renderLoginBtn = () => {
     switch(isSignedIn) {
       case null:
-        return <div>Unknown</div>
+        return null;
       case true:
-        return <div>Hello user !</div>
+        return (
+          <button
+            className="ui red google button"
+            onClick={makeSignOut}
+          >
+          <i className="google icon" />
+            Sign Out
+          </button>
+        );
       default:
-        return <div>Sign In</div>
+        return (
+          <button
+            className="ui red google button"
+            onClick={makeSignIn}
+          >
+          <i className="google icon" />
+            Sign In
+          </button>
+        );
     }
   };
 
