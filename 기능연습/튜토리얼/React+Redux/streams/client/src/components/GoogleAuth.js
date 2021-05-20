3076af0c8027264cb4c055ead55ca4dfbbdb94f5
taraczel <delphi5281@gmail.com>
2021-05-20 22:55:24 +0900

    udemy react+redux - 구글 인증기능 활성화

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/GoogleAuth.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/GoogleAuth.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  useEffect(() => {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '1026008969674-urgnfp54da0a0rot6ggevesnehkqndas.apps.googleusercontent.com',
+        scope: 'email'
+      }).then(() => {
+        const auth = window.gapi.auth2.getAuthInstance();
+        setIsSignedIn(auth.isSignedIn.get());
+      });
+    });
+  }, []);
+
+  const renderLoginBtn = () => {
+    switch(isSignedIn) {
+      case null:
+        return <div>Unknown</div>
+      case true:
+        return <div>Hello user !</div>
+      default:
+        return <div>Sign In</div>
+    }
+  };
+
   return(
     <div>
-      <h1>GoogleAuth</h1>
+      {renderLoginBtn()}
     </div>
   );
 };
