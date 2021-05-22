e0854a414ed272ebf9384fa56b08f524df3722c9
taraczel <delphi5281@gmail.com>
2021-05-23 00:32:49 +0900

    udemy react+redux - 스트리밍 앱 - 헬퍼함수를 통한 input 활성화

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamCreate.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 const SteamCreate = () => {
+  const renderInput = ({ input }) => {
+    return <input { ...input }/>
+  };
+
   return (
     <div>
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={renderInput} />
+        <Field name="description" component={renderInput} />
       </form>
     </div>
   );
