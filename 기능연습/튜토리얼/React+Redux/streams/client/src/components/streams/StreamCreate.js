511785dad5f593963448f8f52ee9384d92f79882
taraczel <delphi5281@gmail.com>
2021-05-24 00:17:56 +0900

    udemy react+redux - 스트리밍 앱 - api 통신기능 구현중, 일부 파일 디렉토리 변경

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamCreate.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { createStreams } from '../../actions';
 
 const renderError = ({ error, touched }) => {
   if (touched && error) {
