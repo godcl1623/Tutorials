f89e9877e62686de3b7cb87fd7d8a09ee583b3fd
taraczel <delphi5281@gmail.com>
2021-05-26 01:56:44 +0900

    udemy react+redux - 스트리밍 앱 - 데이터 편집 기능 구현

--- 기능연습/튜토리얼/React+Redux/streams/client/src/actions/index.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/actions/index.js
@@ -32,8 +32,9 @@ export const fetchAStream = id => async dispatch => {
 }
 
 export const editStream = (id, formValue) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValue);
+  const response = await streams.patch(`/streams/${id}`, formValue);
   dispatch({type: 'EDIT_STREAM', payload: response.data});
+  history.push('/');
 }
 
 export const deleteStream = id => async dispatch => {
--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamEdit.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamEdit.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { fetchStreams } from '../../actions';
+import { fetchStreams, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = ({ match, stream, fetchStreams }) => {
+const StreamEdit = ({ match, stream, fetchStreams, editStream }) => {
   useEffect(() => {
     fetchStreams(match.params.id);
   }, [fetchStreams, match.params.id]);
+
+  const onEditStream = formValues => {
+    editStream(match.params.id, formValues);
+  }
 
   if (!stream) {
     return <div><h1>Loading...</h1></div>;
   } else {
     return (
       <div>
-        {stream.title}
+        <h3>방송 편집</h3>
+        <StreamForm
+          initialValues={{title: stream.title, description: stream.description}}
+          onSubmitData={onEditStream}
+        />
       </div>
     );
   }
@@ -22,4 +31,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStreams })(StreamEdit);
+export default connect(mapStateToProps, { fetchStreams, editStream })(StreamEdit);
