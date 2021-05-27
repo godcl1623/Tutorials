d5be7875124e44d1cd6ca6a492edd301198200b3
taraczel <delphi5281@gmail.com>
2021-05-28 00:52:08 +0900

    udemy react+redux - 스트리밍 앱 - 스트리밍 설정

--- 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamShow.js
+++ 기능연습/튜토리얼/React+Redux/streams/client/src/components/streams/StreamShow.js
@@ -1,21 +1,36 @@
 import React, { useEffect } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchAStream } from '../../actions';
 
-const StreamShow = ({ fetchAStream, match, ...props }) => {
+const StreamShow = ({ fetchAStream, match, stream, ...props }) => {
+
+  const videoRef = React.createRef();
+
   useEffect(() => {
     fetchAStream(match.params.id);
-  }, [fetchAStream, match.params.id]);
+    const buildPlayer = () => {
+      const player = flv.createPlayer({
+        type: 'flv',
+        url: `http://localhost:8000/live/${match.params.id}.flv`
+      });
+      if (player || !stream) {
+        return;
+      }
+      player.attachMediaElement(videoRef.current);
+      player.load();
+    };
+    buildPlayer();
+  }, [fetchAStream, match.params.id, stream, videoRef]);
 
-  const { title, description} = props.stream;
-
-  if (!props.stream) {
+  if (!stream) {
     return <div><h1>Loading...</h1></div>;
   }
   return (
     <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
+      <video ref={videoRef} style={{width: '100%'}} controls />
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
     </div>
   );
 };
