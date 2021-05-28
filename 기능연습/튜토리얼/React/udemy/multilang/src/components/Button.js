384315524ee6c37b3b2da7e171a09b5342e3f2b4
taraczel <delphi5281@gmail.com>
2021-05-29 00:21:14 +0900

    udemy react context - useContext를 이용한 하드코딩 번역 구현

--- 기능연습/튜토리얼/React/udemy/multilang/src/components/Button.js
+++ 기능연습/튜토리얼/React/udemy/multilang/src/components/Button.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import LanguageContext from './contexts/LanguageContext';
 
 const Button = () => {
+  const context = useContext(LanguageContext);
+
+  const btnText = () => {
+    switch(context) {
+      case 'korean':
+        return '제출';
+      case 'english':
+        return 'Submit';
+      case 'japanese':
+        return '提出';
+      default:
+        return 'Loading';
+    }
+  };
+
   return(
-    <button className="ui button primary">제출</button>
+    <button className="ui button primary">{btnText()}</button>
   );
 };
 
--- 기능연습/튜토리얼/React/udemy/multilang/src/components/Field.js
+++ 기능연습/튜토리얼/React/udemy/multilang/src/components/Field.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import LanguageContext from './contexts/LanguageContext';
 
 const Field = () => {
+  const context = useContext(LanguageContext);
+
+  const labelText = () => {
+    switch(context) {
+      case 'korean':
+        return '문장';
+      case 'english':
+        return 'Sentence';
+      case 'japanese':
+        return '文章';
+      default:
+        return 'Loading';
+    }
+  };
+
   return(
     <div className="ui field">
-      <label>문장:</label>
+      <label>{labelText()}:</label>
       <input />
     </div>
   );
