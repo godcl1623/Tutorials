57e0b742d0e77f5cd97024c5a68a1d6608c3f642
taraczel <delphi5281@gmail.com>
2021-05-31 00:06:33 +0900

    생활코딩 - NodeJS - 404페이지 구현, 로컬 폴더 내 파일목록 읽기 구현

--- 기능연습/튜토리얼/NodeJS/웹앱/main.js
+++ 기능연습/튜토리얼/NodeJS/웹앱/main.js
@@ -4,40 +4,66 @@ const url = require('url');
 
 const app = http.createServer((request, response) => {
   let _url = request.url;
-  const queryData = url.parse(_url, true).query;
+  let queryData = url.parse(_url, true).query;
+  let { pathname } = url.parse(_url, true);
   let title = queryData.id;
-  if (_url === '/') {
-    title = 'Welcome';
-  }
-  if (_url === '/favicon.ico') {
+
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      title = 'Welcome';
+      const data = 'Hello, NodeJS !!';
+      const template = `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${data}
+            </p>
+          </body>
+        </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`./src/${queryData.id}`, 'utf8', (err, data) => {
+        const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${data}
+              </p>
+            </body>
+          </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
     response.writeHead(404);
-    response.end();
-    return;
+    response.end('404: Page Not Found');
   }
-  response.writeHead(200);
-  fs.readFile(`./src/${queryData.id}`, 'utf8', (err, data) => {
-    const template = `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${data}
-          </p>
-        </body>
-      </html>
-    `;
-    response.end(template);
-  });
   // response.end(fs.readFileSync(__dirname + _url));
 });
 app.listen(3000);
