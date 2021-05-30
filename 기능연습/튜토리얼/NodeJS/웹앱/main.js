589d513e1aef7c5d5afb2365948a9bb58e0b6fd3
taraczel <delphi5281@gmail.com>
2021-05-31 00:58:33 +0900

    생활코딩 - NodeJS - 파일 리스트를 불러와서 html 리스트 구현

--- 기능연습/튜토리얼/NodeJS/웹앱/main.js
+++ 기능연습/튜토리얼/NodeJS/웹앱/main.js
@@ -1,69 +1,69 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const { testElement } = require('domutils');
 
 const app = http.createServer((request, response) => {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
   let { pathname } = url.parse(_url, true);
   let title = queryData.id;
-
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-      title = 'Welcome';
-      const data = 'Hello, NodeJS !!';
-      const template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${data}
-            </p>
-          </body>
-        </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
+  fs.readdir('./src', (error, filelist) => {
+    const list = filelist.map(element => {
+      return `<li><a href="/?id=${element}">${element}</a></li>`;
+    }).join(' ');
+    if (pathname === '/') {
+      if (queryData.id === undefined) {
+          title = 'Welcome';
+          const data = 'Hello, NodeJS !!';
+          const template = `
+            <!doctype html>
+            <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                  ${list}
+                </ol>
+                <h2>${title}</h2>
+                <p>${data}
+                </p>
+              </body>
+            </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      } else {
+        fs.readFile(`./src/${queryData.id}`, 'utf8', (err, data) => {
+          const template = `
+            <!doctype html>
+            <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                  ${list}
+                </ol>
+                <h2>${title}</h2>
+                <p>${data}
+                </p>
+              </body>
+            </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
     } else {
-      fs.readFile(`./src/${queryData.id}`, 'utf8', (err, data) => {
-        const template = `
-          <!doctype html>
-          <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ol>
-              <h2>${title}</h2>
-              <p>${data}
-              </p>
-            </body>
-          </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+      response.writeHead(404);
+      response.end('404: Page Not Found');
     }
-  } else {
-    response.writeHead(404);
-    response.end('404: Page Not Found');
-  }
-  // response.end(fs.readFileSync(__dirname + _url));
+  });
 });
 app.listen(3000);
