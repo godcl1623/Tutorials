64668983e4b0424a56ab5f4425488c28f937f5b9
taraczel <delphi5281@gmail.com>
2021-05-31 01:20:02 +0900

    생활코딩 - NodeJS - 함수를 통한 코드 재사용

--- 기능연습/튜토리얼/NodeJS/웹앱/main.js
+++ 기능연습/튜토리얼/NodeJS/웹앱/main.js
@@ -1,68 +1,62 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const { testElement } = require('domutils');
 
 const app = http.createServer((request, response) => {
-  let _url = request.url;
-  let queryData = url.parse(_url, true).query;
-  let { pathname } = url.parse(_url, true);
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const { pathname } = url.parse(_url, true);
   let title = queryData.id;
   fs.readdir('./src', (error, filelist) => {
-    const list = filelist.map(element => {
-      return `<li><a href="/?id=${element}">${element}</a></li>`;
-    }).join(' ');
+    const list = filelist
+      .map(element => {
+        return `<li><a href="/?id=${element}">${element}</a></li>`;
+      })
+      .join(' ');
+
+    const template = (title, list, article) => {
+      return `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              ${list}
+            </ol>
+            ${article}
+          </body>
+        </html>
+      `;
+    };
+
+    const article = (title, data) => {
+      return `
+        <h2>${title}</h2>
+        <p>${data}</p>
+      `;
+    };
+
+    const showResponse = (code, result) => {
+      response.writeHead(code);
+      response.end(result);
+    };
+
     if (pathname === '/') {
       if (queryData.id === undefined) {
-          title = 'Welcome';
-          const data = 'Hello, NodeJS !!';
-          const template = `
-            <!doctype html>
-            <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                  ${list}
-                </ol>
-                <h2>${title}</h2>
-                <p>${data}
-                </p>
-              </body>
-            </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+        title = 'Welcome';
+        const data = 'Hello, NodeJS !!';
+        showResponse(200, template(title, list, article(title, data)));
       } else {
         fs.readFile(`./src/${queryData.id}`, 'utf8', (err, data) => {
-          const template = `
-            <!doctype html>
-            <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                  ${list}
-                </ol>
-                <h2>${title}</h2>
-                <p>${data}
-                </p>
-              </body>
-            </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+          showResponse(200, template(title, list, article(title, data)));
         });
       }
     } else {
-      response.writeHead(404);
-      response.end('404: Page Not Found');
+      showResponse(404, '404: Page Not Found');
     }
   });
 });
