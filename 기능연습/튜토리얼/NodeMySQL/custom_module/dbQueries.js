26608c955f8482e9175e8880f494b9737be2471b
taraczel <delphi5281@gmail.com>
2021-06-03 00:10:30 +0900

    생활코딩 - Node + MySQL - 모듈 분리를 통한 코드 리팩토링

---
+++ 기능연습/튜토리얼/NodeMySQL/custom_module/dbQueries.js
@@ -0,0 +1,150 @@
+const qs = require('querystring');
+const db = require('./db');
+const tools = require('./customTools');
+
+exports.home = (response, queryData) => {
+  db.query('select * from topic', (error, table) => {
+    if (error) throw error;
+    const title = 'Welcome';
+    const data = 'Hello, NodeJS !!';
+    response.writeHead(200);
+    response.end(
+      tools.template(
+        title,
+        tools.list(table),
+        tools.article(title, data),
+        tools.control(title, queryData)
+      )
+    );
+  });
+};
+
+exports.specific = (response, queryData) => {
+  db.query('select * from topic', (error, table) => {
+    if (error) throw error;
+    db.query(
+      'select * from topic left join author on topic.author_id=author.id where topic.id=?',
+      [queryData.id],
+      (error2, tabledata) => {
+        if (error2) throw error;
+        const { title, description: desc, name } = tabledata[0];
+        response.writeHead(200);
+        response.end(
+          tools.template(
+            title,
+            tools.list(table),
+            tools.article(title, desc, name),
+            tools.control(queryData.id, queryData)
+          )
+        );
+      }
+    );
+  });
+};
+
+exports.createForm = (response, queryData) => {
+  db.query('select * from topic', (error, table) => {
+    if (error) throw error;
+    db.query('select * from author', (error2, authors) => {
+      if (error2) throw error2;
+      response.writeHead(200);
+      response.end(
+        tools.template(
+          'create',
+          tools.list(table),
+          tools.form('create', '', '', '', tools.option(authors)),
+          tools.control(queryData.id, queryData)
+        )
+      );
+    });
+  });
+};
+
+exports.createProcess = (request, response) => {
+  let body = '';
+  request.on('data', data => {
+    body += data;
+  });
+  request.on('end', () => {
+    const post = qs.parse(body);
+    const { title, description: desc, author } = post;
+    db.query(
+      'insert into topic (title, description, created, author_id) values(?, ?, NOW(), ?)',
+      [title, desc, author],
+      (error, table) => {
+        if (error) throw error;
+        response.writeHead(302, {
+          Location: `/?id=${table.insertId}`
+        });
+        response.end();
+      }
+    );
+  });
+};
+
+exports.updateForm = (response, queryData) => {
+  db.query('select * from topic', (error, table) => {
+    if (error) throw error;
+    db.query('select * from topic where id=?', [queryData.id], (error2, tabledata) => {
+      if (error2) throw error2;
+      db.query('select * from author', (error3, authors) => {
+        if (error3) throw error3;
+        const { id, title, description: desc, author_id: authorId } = tabledata[0];
+        response.writeHead(200);
+        response.end(
+          tools.template(
+            'update',
+            tools.list(table),
+            tools.form('update', id, title, desc, tools.option(authors, authorId)),
+            ''
+          )
+        );
+      });
+    });
+  });
+};
+
+exports.updateProcess = (request, response) => {
+  let body = '';
+  request.on('data', data => {
+    body += data;
+  });
+  request.on('end', () => {
+    const post = qs.parse(body);
+    const { title, description: desc, id, author } = post;
+    db.query(
+      'UPDATE topic SET title=?, description=?, author_id=? WHERE id=?',
+      [title, desc, author, id],
+      (error, modifiedData) => {
+        if (error) throw error;
+        response.writeHead(302, {
+          Location: `/?id=${id}`
+        });
+        response.end();
+      }
+    );
+  });
+};
+
+exports.erase = (request, response) => {
+  let body = '';
+  request.on('data', data => {
+    body += data;
+  });
+  request.on('end', () => {
+    const post = qs.parse(body);
+    const { id } = post;
+    db.query(`DELETE FROM topic WHERE id=?`, [id], (error, updatedTable) => {
+      if (error) throw error;
+      response.writeHead(302, {
+        Location: '/'
+      });
+      response.end();
+    });
+  });
+};
+
+exports.notFound = response => {
+  response.writeHead(404);
+  response.end('404: Page Not Found');
+}
