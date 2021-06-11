3017cba5445cc0556132aac0106a089db0a35b3c
taraczel <delphi5281@gmail.com>
2021-06-11 18:30:18 +0900

    React 습작 - 서버 통신 예외처리 완료

--- 습작/NodeJS/serv-prac/custom_modules/dbQueries.js
+++ 습작/NodeJS/serv-prac/custom_modules/dbQueries.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 module.exports = {
-  template: () => {
+  menu() {
     return `
       <!doctype html>
       <html>
@@ -20,5 +20,100 @@ module.exports = {
         </body>
       </html>
     `;
+  },
+
+  memberList(req, res) {
+    db.query('select * from memberform', (error, table) => {
+      if (error) throw error;
+      if (table.length === 0) this.notFound(req, res);
+      const newTable = [...table];
+      const arrangedData = newTable
+        .map(member => {
+          return `
+          <p>
+            <a href="/member/${member.id}">[${member.id}] ${member.family_name}${member.name}</a>
+          </p>
+        `;
+        })
+        .join(' ');
+      res.send(arrangedData);
+    });
+  },
+
+  individual(req, res) {
+    db.query('select * from memberform where id=?', [req.id], (error, member) => {
+      if (error) throw error;
+      if (member.length === 0) this.notFound(req, res);
+      const newMember = [...member];
+      const columns = [
+        'id',
+        'name',
+        'family_name',
+        'gender',
+        'email',
+        'source',
+        'interests',
+        'favorite_time'
+      ];
+      const arrangedData = newMember
+        .map(data => {
+          return columns
+            .map(column => {
+              return `
+            <p>
+              ${column}: ${data[column]}
+            </p>
+          `;
+            })
+            .join(' ');
+        })
+        .join(' ');
+      res.send(arrangedData);
+    });
+  },
+
+  newsList(req, res) {
+    db.query('select * from newslist', (error, table) => {
+      if (error) throw error;
+      if (table.length === 0) this.notFound(req, res);
+      const newTable = [...table];
+      const arrangedData = newTable
+        .map(news => {
+          return `
+          <p>
+            <a href="/news/${news.id}">[${news.id}] ${news.title}</a>
+          </p>
+        `;
+        })
+        .join(' ');
+      res.send(arrangedData);
+    });
+  },
+
+  eachNews(req, res) {
+    db.query('select * from newslist where id=?', [req.id], (error, news, next) => {
+      if (error) next(error);
+      if (news.length === 0) this.notFound(req, res);
+      const newNews = [...news];
+      const columns = ['id', 'title', 'contents'];
+      const arrangedData = newNews
+        .map(data => {
+          return columns
+            .map(column => {
+              return `
+            <p>
+              ${column}: ${data[column]}
+            </p>
+          `;
+            })
+            .join(' ');
+        })
+        .join(' ');
+      res.send(arrangedData);
+    });
+  },
+
+  notFound(req, res) {
+    res.status(404).send('Page Not Found !');
   }
 };
--- 습작/NodeJS/serv-prac/server.js
+++ 습작/NodeJS/serv-prac/server.js
@@ -4,99 +4,36 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 const query = require('./custom_modules/dbQueries');
-const db = require('./custom_modules/db');
 
 const app = express();
 const port = 3001;
 app.use(cors(), bodyParser.json(), compression(), helmet());
 
-app.get('/', (req, res) => res.send(query.template()));
+app.get('/', (req, res) => res.send(query.menu()));
 
 app.get('/member', (req, res) => {
-  db.query('select * from memberform', (error, table) => {
-    if (error) throw error;
-    const newTable = [...table];
-    const arrangedData = newTable
-      .map(member => {
-        return `
-        <p>
-          <a href="/member/${member.id}">[${member.id}] ${member.family_name}${member.name}</a>
-        </p>
-      `;
-      })
-      .join(' ');
-    res.send(arrangedData);
-  });
+  query.memberList(req, res);
 });
 
 app.get('/member/:id', (req, res) => {
-  db.query('select * from memberform where id=?', [req.params.id], (error, member) => {
-    if (error) throw error;
-    const newMember = [...member];
-    const columns = [
-      'id',
-      'name',
-      'family_name',
-      'gender',
-      'email',
-      'source',
-      'interests',
-      'favorite_time'
-    ];
-    const arrangedData = newMember
-      .map(data => {
-        return columns
-          .map(column => {
-            return `
-          <p>
-            ${column}: ${data[column]}
-          </p>
-        `;
-          })
-          .join(' ');
-      })
-      .join(' ');
-    res.send(arrangedData);
-  });
+  query.individual(req.params, res);
 });
 
 app.get('/news', (req, res) => {
-  db.query('select * from newslist', (error, table) => {
-    if (error) throw error;
-    const newTable = [...table];
-    const arrangedData = newTable
-      .map(news => {
-        return `
-        <p>
-          <a href="/news/${news.id}">[${news.id}] ${news.title}</a>
-        </p>
-      `;
-      })
-      .join(' ');
-    res.send(arrangedData);
-  });
+  query.newsList(req, res);
 });
 
 app.get('/news/:id', (req, res) => {
-  db.query('select * from newslist where id=?', [req.params.id], (error, news) => {
-    if (error) throw error;
-    const newNews = [...news];
-    const columns = ['id', 'title', 'contents'];
-    const arrangedData = newNews
-      .map(data => {
-        return columns
-          .map(column => {
-            return `
-          <p>
-            ${column}: ${data[column]}
-          </p>
-        `;
-          })
-          .join(' ');
-      })
-      .join(' ');
-    res.send(arrangedData);
-  });
+  query.eachNews(req.params, res);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page Not Found !');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something Broke !');
 });
 
 app.listen(port, () => console.log(`server is running at port ${port}`));
