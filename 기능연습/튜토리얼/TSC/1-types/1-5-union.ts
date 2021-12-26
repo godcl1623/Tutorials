847597290ab21bea2a71a0017968ee15bcaa6d77
godcl1623 <delphi5281@gmail.com>
2021-12-27 00:27:12 +0900

    Tutorials - main - union 타입 강의록 + 퀴즈 내 답안

--- 기능연습/튜토리얼/TSC/1-types/1-5-union.ts
+++ 기능연습/튜토리얼/TSC/1-types/1-5-union.ts
@@ -16,27 +16,47 @@
   type LoginResult = 'success' | 'fail';
   function login(result: LoginResult) {
     // do something
-  };
+  }
   // 정답:
   type SuccessState = {
     response: {
       body: string;
-    }
+    };
   };
   type FailState = {
     reason: string;
   };
   type LoginState = SuccessState | FailState;
   function login(id: string, pwd: string): Promise<LoginState> {
-    // do something
-    return {
-      response: {
-        body: 'logged in!',
+    return new Promise((resolve, reject) => {
+      if (id === pwd) {
+        resolve({
+          response: {
+            body: 'logged in'
+          }
+        });
+      } else {
+        reject(new Error('blah blah'));
       }
-    }
-  };
+    });
+  }
+  // function login(): LoginState {
+  //   return {
+  //     response: {
+  //       body: 'logged in !'
+  //     }
+  //   };
+  // }
 
   // 퀴즈: printLoginState(state: LoginState)
   // success -> 성공 response body
   // fail -> 실패 이유
+
+  const printLoginState = (state: LoginState) => {
+    if (state.response !== undefined) {
+      console.log('🎉', { response: { body: 'login success !' } });
+    } else {
+      console.log('😭', { reason: 'invalid info' });
+    }
+  };
 }
