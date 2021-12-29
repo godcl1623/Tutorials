63fded24daec4da3c29b40325ad838782e3a31fd
godcl1623 <delphi5281@gmail.com>
2021-12-30 01:01:30 +0900

    Tutorials - main - 추상화 보충: interface 심화

--- 기능연습/튜토리얼/TSC/3-OOP/3-4-abstraction.ts
+++ 기능연습/튜토리얼/TSC/3-OOP/3-4-abstraction.ts
@@ -4,7 +4,17 @@
     hasMilk: boolean;
   };
 
-  class CoffeeMaker {
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CupOfCoffee;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CupOfCoffee;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_PER_SHOTS: number = 15;
     private coffeeBeans: number = 0;
 
@@ -12,25 +22,77 @@
       this.coffeeBeans = coffeeBeans;
     }
 
-    public static makeMaker = (coffeeBeans: number): CoffeeMaker => {
-      return new CoffeeMaker(coffeeBeans);
+    // 이 부분 리턴 타입 잘못 작성하지 않도록 주의 !!!
+    public static makeMaker = (coffeeBeans: number): CoffeeMachine => {
+      return new CoffeeMachine(coffeeBeans);
     };
 
-    makeCoffee = (shots: number): CupOfCoffee => {
-      // if (this.coffeeBeans < shots * CoffeeMaker.BEANS_PER_SHOTS) {
-      //   throw new Error(`Not enough beans. Current: ${this.coffeeBeans}`);
-      // }
-      // this.coffeeBeans -= shots * CoffeeMaker.BEANS_PER_SHOTS;
-      // return {
-      //   shots,
-      //   hasMilk: false
-      // };
-    };
-
-    public setBeans = (beans: number): void => {
+    public fillCoffeeBeans(beans: number): void {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
       this.coffeeBeans = beans;
+    }
+
+    clean(): void {
+      console.log('cleaning the machine...');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_PER_SHOTS) {
+        throw new Error('Not enough coffee beans !');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_PER_SHOTS;
+    }
+
+    private preheat(): void {
+      console.log('heating machine...');
+    }
+
+    extract = (shots: number): CupOfCoffee => {
+      console.log(`Pulling ${shots} shots...`);
+      return {
+        shots,
+        hasMilk: false
+      };
     };
+
+    makeCoffee(shots: number): CupOfCoffee {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
   }
 
-  const maker2 = CoffeeMaker.makeMaker(200);
+  const maker: CoffeeMachine = CoffeeMachine.makeMaker(200);
+  // maker.fillCoffeeBeans(32);
+  // maker.makeCoffee(2);
+  // const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMaker(200);
+  // maker2.fillCoffeeBeans(32);
+  // maker2.makeCoffee(2);
+  // maker2.clean();
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  const user: AmateurUser = new AmateurUser(maker);
+  user.makeCoffee();
+  const barista: ProBarista = new ProBarista(maker);
+  barista.makeCoffee();
 }
