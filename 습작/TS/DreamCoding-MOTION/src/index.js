8c5a9c260f6f7e71e7ee00136894d5804b47399f
godcl1623 <delphi5281@gmail.com>
2022-01-12 23:22:25 +0900

    DCM - mod - 클릭한 메뉴에 따라 다른 양식 표시 구현

--- 습작/TS/DreamCoding-MOTION/src/index.js
+++ 습작/TS/DreamCoding-MOTION/src/index.js
@@ -1,70 +1,95 @@
-/* 1. 모달창 여닫기 */
+var PostCreator = /** @class */ (function () {
+    function PostCreator(menuType) {
+        this.menuType = menuType;
+    }
+    PostCreator.prototype.baseModule = function (ipt) {
+        var $div = document.createElement('div');
+        $div.className = 'input_container del_target';
+        var $label = document.createElement('label');
+        $label.htmlFor = ipt.forVal;
+        $label.textContent = ipt.labelTxt;
+        $div.appendChild($label);
+        return $div;
+    };
+    PostCreator.prototype.mediaPostCreator = function (ipt) {
+        var $input = document.createElement('input');
+        $input.name = ipt.nameVal;
+        $input.className = ipt.classVal;
+        return $input;
+    };
+    PostCreator.prototype.textPostCreator = function (ipt) {
+        var $area = document.createElement('textarea');
+        $area.name = ipt.nameVal;
+        $area.className = ipt.classVal;
+        return $area;
+    };
+    PostCreator.prototype.ctnCreator = function () {
+        var result;
+        if (this.menuType === 'IMAGE' || this.menuType === 'VIDEO') {
+            var basePayload = {
+                forVal: 'URL',
+                labelTxt: 'URL'
+            };
+            var mediaPayload = {
+                nameVal: 'URL',
+                classVal: 'url'
+            };
+            var baseCnt = this.baseModule(basePayload);
+            var mediaPost = this.mediaPostCreator(mediaPayload);
+            baseCnt === null || baseCnt === void 0 ? void 0 : baseCnt.appendChild(mediaPost);
+            result = baseCnt;
+        }
+        else {
+            var basePayload = {
+                forVal: 'Body',
+                labelTxt: 'Body'
+            };
+            var txtPayload = {
+                nameVal: 'Body',
+                classVal: 'body'
+            };
+            var baseCnt = this.baseModule(basePayload);
+            var txtPost = this.textPostCreator(txtPayload);
+            baseCnt === null || baseCnt === void 0 ? void 0 : baseCnt.appendChild(txtPost);
+            result = baseCnt;
+        }
+        return result;
+    };
+    return PostCreator;
+}());
+/* 2. 모달창 여닫기 */
 var motionMenu = document.querySelector('#motion_menu');
 var menuBtns = motionMenu === null || motionMenu === void 0 ? void 0 : motionMenu.querySelectorAll('.menu_items');
 var modalBg = document.querySelector('#modal_bg');
 var modalCloseBtn = modalBg === null || modalBg === void 0 ? void 0 : modalBg.querySelector('.btn_close');
 function modalOpener(btns, target) {
     btns === null || btns === void 0 ? void 0 : btns.forEach(function (btn) {
-        return btn.addEventListener('click', function () {
+        return btn.addEventListener('click', function (e) {
             // null인 경우가 있을 수 있으므로 주의
+            var eTargetToHTML = e.target;
+            var postCreator = new PostCreator(eTargetToHTML.textContent);
+            var $inputCnt = postCreator.ctnCreator();
+            var modalForm = target === null || target === void 0 ? void 0 : target.querySelector('form#form_post');
             target === null || target === void 0 ? void 0 : target.classList.remove('disabled');
+            modalForm === null || modalForm === void 0 ? void 0 : modalForm.appendChild($inputCnt);
         });
     });
 }
 function modalCloser(bg, btn) {
     var targets = [bg, btn];
     targets.forEach(function (target) {
-        return target === null || target === void 0 ? void 0 : target.addEventListener('click', function () {
-            bg === null || bg === void 0 ? void 0 : bg.classList.add('disabled');
+        return target === null || target === void 0 ? void 0 : target.addEventListener('click', function (e) {
+            var eTargetToHTML = e.target;
+            if (eTargetToHTML.id === 'modal_bg' || eTargetToHTML.className === 'btn_close') {
+                var delTarget = bg === null || bg === void 0 ? void 0 : bg.querySelector('.del_target');
+                var modalForm = bg === null || bg === void 0 ? void 0 : bg.querySelector('form#form_post');
+                bg === null || bg === void 0 ? void 0 : bg.classList.add('disabled');
+                if (delTarget) {
+                    modalForm === null || modalForm === void 0 ? void 0 : modalForm.removeChild(delTarget);
+                }
+            }
         });
     });
 }
 modalOpener(menuBtns, modalBg);
 modalCloser(modalBg, modalCloseBtn);
-/* 2. 추가 메뉴 모듈화 */
-var PostCreator = /** @class */ (function () {
-    function PostCreator() {
-        this.modalBody = modalBg === null || modalBg === void 0 ? void 0 : modalBg.querySelector('#modal_body');
-    }
-    PostCreator.prototype.ctnCreator = function (className, labelTxt, forTxt, nameTxt, clsNameTxt) {
-        var result;
-        var $div = document.createElement('div');
-        $div.className = className;
-        if (className === 'close_container') {
-            var $btn = document.createElement('button');
-            $btn.className = 'btn_close';
-            $btn.textContent = '×';
-            $div.appendChild($btn);
-            result = $div;
-        }
-        else if (className === 'input_container') {
-            if (labelTxt && forTxt && nameTxt && clsNameTxt) {
-                var $label = document.createElement('label');
-                $label.htmlFor = forTxt;
-                $label.textContent = labelTxt;
-                var $input = document.createElement('input');
-                $input.name = nameTxt;
-                $input.className = clsNameTxt;
-                $div.appendChild($label);
-                $div.appendChild($input);
-                result = $div;
-            }
-            else {
-                throw new Error('Unexpected error occured: Some of txt values are undefined');
-            }
-        }
-        else if (className === 'btn_container') {
-            var $btn = document.createElement('button');
-            $btn.className = 'btn_add';
-            $btn.textContent = 'ADD';
-            $div.appendChild($btn);
-            result = $div;
-        }
-        return result;
-    };
-    return PostCreator;
-}());
-// const $div = elmCreator('div');
-// const $label = elmCreator('label');
-// const $input = elmCreator('input');
-// const $textarea = elmCreator('textarea');
--- 습작/TS/DreamCoding-MOTION/src/index.ts
+++ 습작/TS/DreamCoding-MOTION/src/index.ts
@@ -1,72 +1,113 @@
-/* 1. 모달창 여닫기 */
+/* 1. 추가 메뉴 모듈화 */
+type BasePayload = {
+  forVal: string;
+  labelTxt: string;
+};
+
+type MediaPayload = {
+  nameVal: BasePayload['forVal'];
+  classVal: string;
+};
+
+type TxtPayload = MediaPayload;
+
+interface IPostCreator {
+  ctnCreator(): HTMLElement;
+}
+
+class PostCreator implements IPostCreator {
+  constructor(protected menuType: string | null) {}
+  
+  protected baseModule(ipt: BasePayload): HTMLElement {
+    const $div = document.createElement('div');
+    $div.className = 'input_container del_target';
+    const $label = document.createElement('label');
+    $label.htmlFor = ipt.forVal;
+    $label.textContent = ipt.labelTxt;
+    $div.appendChild($label);
+    return $div;
+  }
+  
+  protected mediaPostCreator(ipt: MediaPayload): HTMLElement {
+    const $input = document.createElement('input');
+    $input.name = ipt.nameVal;
+    $input.className = ipt.classVal;
+    return $input;
+  }
+  
+  protected textPostCreator(ipt: TxtPayload): HTMLElement {
+    const $area = document.createElement('textarea');
+    $area.name = ipt.nameVal;
+    $area.className = ipt.classVal;
+    return $area;
+  }
+  
+  ctnCreator(): HTMLElement {
+    let result: HTMLElement;
+    if (this.menuType === 'IMAGE' || this.menuType === 'VIDEO') {
+      const basePayload: BasePayload = {
+        forVal: 'URL',
+        labelTxt: 'URL'
+      }
+      const mediaPayload: MediaPayload = {
+        nameVal: 'URL',
+        classVal: 'url'
+      }
+      const baseCnt = this.baseModule(basePayload);
+      const mediaPost = this.mediaPostCreator(mediaPayload);
+      baseCnt?.appendChild(mediaPost);
+      result = baseCnt;
+    } else {
+      const basePayload: BasePayload = {
+        forVal: 'Body',
+        labelTxt: 'Body'
+      }
+      const txtPayload: TxtPayload = {
+        nameVal: 'Body',
+        classVal: 'body'
+      }
+      const baseCnt = this.baseModule(basePayload);
+      const txtPost = this.textPostCreator(txtPayload);
+      baseCnt?.appendChild(txtPost);
+      result = baseCnt;
+    }
+    return result;
+  }
+}
+
+/* 2. 모달창 여닫기 */
 const motionMenu = document.querySelector('#motion_menu');
 const menuBtns = motionMenu?.querySelectorAll('.menu_items');
 const modalBg = document.querySelector('#modal_bg');
 const modalCloseBtn = modalBg?.querySelector('.btn_close');
 function modalOpener(btns: NodeListOf<Element> | undefined, target: Element | null): void {
   btns?.forEach(btn =>
-    btn.addEventListener('click', (): void => {
+    btn.addEventListener('click', (e): void => {
       // null인 경우가 있을 수 있으므로 주의
+      const eTargetToHTML = e.target as HTMLElement;
+      const postCreator: PostCreator = new PostCreator(eTargetToHTML.textContent);
+      const $inputCnt = postCreator.ctnCreator();
+      const modalForm = target?.querySelector('form#form_post');
       target?.classList.remove('disabled');
+      modalForm?.appendChild($inputCnt);
     })
   );
 }
 function modalCloser(bg: Element | null, btn: Element | null | undefined): void {
   const targets = [bg, btn];
   targets.forEach(target =>
-    target?.addEventListener('click', (): void => {
-      bg?.classList.add('disabled');
+    target?.addEventListener('click', (e): void => {
+      const eTargetToHTML = e.target as HTMLElement;
+      if (eTargetToHTML.id === 'modal_bg' || eTargetToHTML.className === 'btn_close') {
+        const delTarget = bg?.querySelector('.del_target');
+        const modalForm = bg?.querySelector('form#form_post');
+        bg?.classList.add('disabled');
+        if (delTarget) {
+          modalForm?.removeChild(delTarget);
+        }
+      }
     })
   );
 }
 modalOpener(menuBtns, modalBg);
 modalCloser(modalBg, modalCloseBtn);
-
-/* 2. 추가 메뉴 모듈화 */
-class PostCreator {
-  protected modalBody = modalBg?.querySelector('#modal_body');
-
-  protected ctnCreator(
-    className: string,
-    labelTxt?: string,
-    forTxt?: string,
-    nameTxt?: string,
-    clsNameTxt?: string
-  ): HTMLElement | undefined {
-    let result: HTMLElement | undefined;
-    const $div = document.createElement('div');
-    $div.className = className;
-    if (className === 'close_container') {
-      const $btn = document.createElement('button');
-      $btn.className = 'btn_close';
-      $btn.textContent = '×';
-      $div.appendChild($btn);
-      result = $div;
-    } else if (className === 'input_container') {
-      if (labelTxt && forTxt && nameTxt && clsNameTxt) {
-        const $label = document.createElement('label');
-        $label.htmlFor = forTxt;
-        $label.textContent = labelTxt;
-        const $input = document.createElement('input');
-        $input.name = nameTxt;
-        $input.className = clsNameTxt;
-        $div.appendChild($label);
-        $div.appendChild($input);
-        result = $div;
-      } else {
-        throw new Error('Unexpected error occured: Some of txt values are undefined');
-      }
-    } else if (className === 'btn_container') {
-      const $btn = document.createElement('button');
-      $btn.className = 'btn_add';
-      $btn.textContent = 'ADD';
-      $div.appendChild($btn);
-      result = $div;
-    }
-    return result;
-  }
-}
-// const $div = elmCreator('div');
-// const $label = elmCreator('label');
-// const $input = elmCreator('input');
-// const $textarea = elmCreator('textarea');
