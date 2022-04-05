2d83a37a9f5965d8bd44e85cd9e9f5ba48ad8660
godcl1623 <delphi5281@gmail.com>
2022-04-05 23:32:50 +0900

    windowing 예시용 프로젝트 생성(수정중)

--- 기능연습/자습/react-prac/src/index.tsx
+++ 기능연습/자습/react-prac/src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-reactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.querySelector('#root')
-);
+const root = ReactDOMClient.createRoot(document.querySelector('#root') as Element);
+root.render(<App />);
